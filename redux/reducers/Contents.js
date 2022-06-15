3d9cd2ab4140ca718627e49a4fae94f07fb478b2
vickyfto <vickyfto19@gmail.com>
2022-06-15 15:23:01 +0700

    feat: consume API contents

--- redux/reducers/Contents.js
+++ redux/reducers/Contents.js
@@ -1,30 +1,28 @@
-import {
-    GET_CONTENTS,
-  } from '../../modules/Constants/ActionTypes';
+import { GET_CONTENTS } from '../../modules/Constants/ActionTypes';
 
-  const INIT_STATE = {
-    contents: [],
-    currentContent: null,
-  };
+const INIT_STATE = {
+  contents: [],
+  currentContent: null,
+};
 
-  const Contents = (state = INIT_STATE, action) => {
-    //console.log(action.payload);
-    switch (action.type) {
-        case GET_CONTENTS: {
-          if(action.loadMore) {
-            return {
-              ...state,
-              contents: [...state.contents, ...action.payload],
-            };
-          } else {
-            return {
-              ...state,
-              contents: action.payload,
-            };
-          }
-        }
-        default:
-          return state;
+const Contents = (state = INIT_STATE, action) => {
+  //console.log(action.payload);
+  switch (action.type) {
+    case GET_CONTENTS: {
+      if (action.loadMore) {
+        return {
+          ...state,
+          contents: [...state.contents, ...action.payload],
+        };
+      } else {
+        return {
+          ...state,
+          contents: action.payload,
+        };
       }
-    };
-    export default Contents;
+    }
+    default:
+      return state;
+  }
+};
+export default Contents;
--- redux/store/index.js
+++ redux/store/index.js
@@ -3,6 +3,10 @@ import { createWrapper } from 'next-redux-wrapper';
 import reducers from '../reducers';
 import { authApi, userApi } from 'api/user';
 import { dashboardApi, engagementApi } from 'api/console';
+import { contentAPI } from 'api/user/content';
+import { notificationAPI } from 'api/user/notification';
+import { commentAPI } from 'api/user/comment';
+import { insightAPI } from 'api/user/insight';
 
 const initStore = () => {
   const store = configureStore({
@@ -13,6 +17,10 @@ const initStore = () => {
         userApi.middleware,
         dashboardApi.middleware,
         engagementApi.middleware,
+        contentAPI.middleware,
+        notificationAPI.middleware,
+        commentAPI.middleware,
+        insightAPI.middleware,
       ]),
   });
   return store;
