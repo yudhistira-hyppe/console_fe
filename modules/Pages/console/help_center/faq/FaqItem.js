434cbb7d5e21df01ce9c64c76798305822d5d197
ekaiqbalv <ekaiqbalvirgiawan@gmail.com>
2022-06-27 12:17:37 +0700

    feat: binding get faq list, add faq category, and add detail faq

--- modules/Pages/console/help_center/faq/FaqItem.js
+++ modules/Pages/console/help_center/faq/FaqItem.js
@@ -1,42 +1,51 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import GridContainer from '@jumbo/components/GridContainer';
-import { Grid, IconButton } from '@material-ui/core';
+import { Grid } from '@material-ui/core';
 import moment from 'moment';
-import CmtDropdownMenu from '@coremat/CmtDropdownMenu';
-import MoreVertIcon from '@material-ui/icons/MoreVert';
-import EditIcon from '@material-ui/icons/Edit';
-import DeleteIcon from '@material-ui/icons/Delete';
+// import CmtDropdownMenu from '@coremat/CmtDropdownMenu';
+// import MoreVertIcon from '@material-ui/icons/MoreVert';
+// import EditIcon from '@material-ui/icons/Edit';
+// import DeleteIcon from '@material-ui/icons/Delete';
 
-const actions = [
-    {
-        label: 'Ubah',
-        icon: <EditIcon/>
-    },
-    {
-        label: 'Hapus',
-        icon: <DeleteIcon />
-    },
-];
+// const actions = [
+//   {
+//     label: 'Ubah',
+//     icon: <EditIcon />,
+//   },
+//   {
+//     label: 'Hapus',
+//     icon: <DeleteIcon />,
+//   },
+// ];
 
-const FaqItem = ({data}) => {
-    const dateObject = moment(data.created_at, "YYYY-MM-DD");
-    return (
-        <GridContainer>
-            <Grid item xs={12} sm={6} md={7}>
-                {data.title}
-            </Grid>
-            <Grid item xs={12} sm={4} md={3}>
-                {dateObject.format('DD MMM')}
-            </Grid>
-            <Grid item xs={12} sm={2} md={2}>
-                <CmtDropdownMenu
-                    TriggerComponent={<IconButton size="small"><MoreVertIcon /></IconButton>}
-                    items={actions}
-                    onItemClick={() => null}
-                />
-            </Grid>
-        </GridContainer>
-    )
-  };
-  
-  export default FaqItem;
+const FaqItem = ({ data }) => {
+  const dateObject = moment(data.datetime, 'YYYY-MM-DD');
+  return (
+    <GridContainer>
+      <Grid item xs={12} sm={6} md={7}>
+        {data.title}
+      </Grid>
+      <Grid item xs={12} sm={4} md={3}>
+        {dateObject.format('DD MMM')}
+      </Grid>
+      {/* <Grid item xs={12} sm={2} md={2}>
+        <CmtDropdownMenu
+          TriggerComponent={
+            <IconButton size="small">
+              <MoreVertIcon />
+            </IconButton>
+          }
+          items={actions}
+          onItemClick={() => null}
+        />
+      </Grid> */}
+    </GridContainer>
+  );
+};
+
+FaqItem.propTypes = {
+  data: PropTypes.object,
+};
+
+export default FaqItem;
--- modules/Pages/console/help_center/faq/index.js
+++ modules/Pages/console/help_center/faq/index.js
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from 'react';
-import { Box, Button, Dialog, Grid, DialogTitle, DialogContent, DialogContentText, DialogActions, TextField, Typography } from '@material-ui/core';
+import PropTypes from 'prop-types';
+import { Box, Button, Dialog, Grid, DialogContent, DialogActions, TextField, Typography } from '@material-ui/core';
 import makeStyles from '@material-ui/core/styles/makeStyles';
 import Head from 'next/head';
 import clsx from 'clsx';
-import { useDispatch, useSelector } from 'react-redux';
 import PageContainer from '@jumbo/components/PageComponents/layouts/PageContainer';
 import GridContainer from '@jumbo/components/GridContainer';
 import CmtCard from '@coremat/CmtCard';
@@ -11,50 +11,49 @@ import CmtCardHeader from '@coremat/CmtCard/CmtCardHeader';
 import CmtCardContent from '@coremat/CmtCard/CmtCardContent';
 import FaqItem from './FaqItem';
 import AddIcon from '@material-ui/icons/Add';
-import EditIcon from '@material-ui/icons/Edit';
-import DeleteIcon from '@material-ui/icons/Delete';
+// import EditIcon from '@material-ui/icons/Edit';
+// import DeleteIcon from '@material-ui/icons/Delete';
 import CloseIcon from '@material-ui/icons/Close';
-
-import { postNewFaq, setSelectedFaq } from 'redux/actions/helpCenterAction';
 import { useRouter } from 'next/router';
+import { useCreateFaqAndInfoMutation, useGetListFaqOrInfoByTypeQuery } from 'api/console/helpCenter/faqAndInfo';
 
 const useStyles = makeStyles(() => ({
-    root: {
-      '& .page-header': {
-        marginBottom: 0
-      }
+  root: {
+    '& .page-header': {
+      marginBottom: 0,
     },
-    header: {
-      borderBottom: '1px solid #ddd',
-      marginBottom: 24,
-      padding: '8px 24px',
-      '& .makeStyles-actionMenu-106 button': {
-          width: 28
-      }
+  },
+  header: {
+    borderBottom: '1px solid #ddd',
+    marginBottom: 24,
+    padding: '8px 24px',
+    '& .makeStyles-actionMenu-106 button': {
+      width: 28,
     },
-    headerButton: {
-      borderBottom: '1px solid #ddd',
-      paddingBottom: 16,
-    },
-    subtitle: {
-      color: 'rgba(0, 0, 0, 0.6)',
-      marginBottom: 24
-    }
+  },
+  headerButton: {
+    borderBottom: '1px solid #ddd',
+    paddingBottom: 16,
+  },
+  subtitle: {
+    color: 'rgba(0, 0, 0, 0.6)',
+    marginBottom: 24,
+  },
 }));
 
 const actions = [
-    {
-        label: 'Tambah',
-        icon: <AddIcon/>
-    },
-    {
-        label: 'Ubah',
-        icon: <EditIcon/>
-    },
-    {
-        label: 'Hapus',
-        icon: <DeleteIcon />
-    },
+  {
+    label: 'Tambah',
+    icon: <AddIcon />,
+  },
+  // {
+  //   label: 'Ubah',
+  //   icon: <EditIcon />,
+  // },
+  // {
+  //   label: 'Hapus',
+  //   icon: <DeleteIcon />,
+  // },
 ];
 
 const breadcrumbs = [
@@ -63,46 +62,67 @@ const breadcrumbs = [
   { label: 'FAQ', isActive: true },
 ];
 
-const ConsoleFaqComponent = () => {
-  const classes = useStyles();
-  const dispatch = useDispatch();
-  const router = useRouter();
-  const { listFaq } = useSelector((state) => state.helpCenterReducers);
-  const [showDialogAddCategory,setShowDialogAddCategory] = useState(false);
+const DialogAddCategory = ({ open, onClose, onSubmit }) => {
+  const [categoryTitle, setCategoryTitle] = useState('');
 
-  const handleMoreMenuClick = (val,faq) => {
-    dispatch(setSelectedFaq(faq));
-    if(val.label == 'Tambah') {
-      router.push('/console/help_center/faq/add')
+  return (
+    <Dialog open={open} onClose={onClose} aria-labelledby="form-dialog-title">
+      <DialogContent>
+        <TextField
+          margin="dense"
+          label="Kategori"
+          type="text"
+          fullWidth
+          variant="outlined"
+          value={categoryTitle}
+          onChange={(e) => setCategoryTitle(e.target.value)}
+        />
+      </DialogContent>
+      <DialogActions>
+        <Button variant="contained" startIcon={<AddIcon />} color="primary" onClick={() => onSubmit(categoryTitle)}>
+          Tambah
+        </Button>
+        <Button startIcon={<CloseIcon />} color="default" onClick={onClose}></Button>
+      </DialogActions>
+    </Dialog>
+  );
+};
+
+DialogAddCategory.propTypes = {
+  open: PropTypes.bool,
+  onClose: PropTypes.func,
+  onSubmit: PropTypes.func,
+};
+
+const ConsoleFaqComponent = () => {
+  const router = useRouter();
+  const classes = useStyles();
+  const { data } = useGetListFaqOrInfoByTypeQuery('faq');
+  const [addFaqCategory] = useCreateFaqAndInfoMutation();
+  const [faqList, setFaqList] = useState([]);
+  const [showDialogAddCategory, setShowDialogAddCategory] = useState(false);
+
+  useEffect(() => {
+    if (data) {
+      setFaqList(data?.data);
+    } else {
+      setFaqList([]);
+    }
+  }, [data]);
+
+  const onClickMoreMenu = (val, info) => {
+    if (val.label == 'Tambah') {
+      router.push({
+        pathname: '/console/help_center/faq/add',
+        query: { id: info._id },
+      });
     }
   };
 
-  const submitNewCategory = (val) => {
-    dispatch(postNewFaq(val));
+  const onSubmitNewCategory = (newCategory) => {
+    addFaqCategory({ kategori: newCategory, tipe: 'faq' });
     setShowDialogAddCategory(false);
-  }
-  
-  const createNewFaqCategory = () => {
-    setShowDialogAddCategory(true)
-  }
-
-  const DialogAddCategory = ({open,handleClose,handleSubmit}) => {
-    const [categoryTitle,setCategoryTitle] = useState("");
-
-    return (
-      <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title">
-        <DialogContent>
-          <TextField autoFocus margin="dense" label="Kategori" type="text" fullWidth variant="outlined" value={categoryTitle} onChange={(e) => setCategoryTitle(e.target.value)} />
-        </DialogContent>
-        <DialogActions>
-          <Button variant='contained' startIcon={<AddIcon />} color="primary" onClick={() => handleSubmit(categoryTitle)}>
-            Tambah
-          </Button>
-          <Button startIcon={<CloseIcon />} color="default" onClick={handleClose}></Button>
-        </DialogActions>
-      </Dialog>
-    )
-  }
+  };
 
   return (
     <>
@@ -110,35 +130,46 @@ const ConsoleFaqComponent = () => {
         <title key="title">Hyppe-Console :: FAQ</title>
       </Head>
       <PageContainer className={classes.root} heading="FAQ" breadcrumbs={breadcrumbs}>
-        <Typography className={classes.subtitle} component="div" variant="h5">{listFaq.length} Kategori</Typography>
+        <Typography className={classes.subtitle} component="div" variant="h5">
+          {faqList.length} Kategori
+        </Typography>
         <GridContainer>
           <Grid item xs={12} sm={12} md={12}>
             <Box className={classes.headerButton}>
-              <Button variant='contained' startIcon={<AddIcon />} color="primary" onClick={createNewFaqCategory}>Buat Baru</Button>
+              <Button
+                variant="contained"
+                startIcon={<AddIcon />}
+                color="primary"
+                onClick={() => setShowDialogAddCategory(true)}>
+                Buat Baru
+              </Button>
             </Box>
           </Grid>
-
-            {listFaq && listFaq.map((faq,index) => 
-                <Grid item xs={12} sm={6} md={4} key={index}>
-                    <CmtCard>
-                        <CmtCardHeader
-                            className={clsx(classes.headerRoot, classes.header)}
-                            title={faq.title}
-                            actionsPos="top-corner"
-                            actions={actions}
-                            actionHandler={(val) => handleMoreMenuClick(val,faq)}/>
-                        <CmtCardContent>
-                           {faq.details && faq.details.map((detail,detIdx) => 
-                                <FaqItem data={detail} key={detIdx}/>
-                           )}
-                        </CmtCardContent>
-                    </CmtCard>
-                </Grid>
-            )}
-        </GridContainer>    
+          {faqList &&
+            faqList.map((faq, index) => (
+              <Grid item xs={12} sm={6} md={4} key={index}>
+                <CmtCard>
+                  <CmtCardHeader
+                    className={clsx(classes.headerRoot, classes.header)}
+                    title={faq.kategori}
+                    actionsPos="top-corner"
+                    actions={actions}
+                    actionHandler={(val) => onClickMoreMenu(val, faq)}
+                  />
+                  <CmtCardContent>
+                    {faq.replydata?.length > 0 &&
+                      faq.replydata.map((detail, detIdx) => <FaqItem data={detail} key={detIdx} />)}
+                  </CmtCardContent>
+                </CmtCard>
+              </Grid>
+            ))}
+        </GridContainer>
       </PageContainer>
-
-      <DialogAddCategory open={showDialogAddCategory} handleClose={() => setShowDialogAddCategory(false)} handleSubmit={submitNewCategory} />
+      <DialogAddCategory
+        open={showDialogAddCategory}
+        onClose={() => setShowDialogAddCategory(false)}
+        onSubmit={onSubmitNewCategory}
+      />
     </>
   );
 };
