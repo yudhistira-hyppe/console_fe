0fbedc1da6ed73ba0f224f3d2457ffc07271470d
vickyfto <vickyfitrio19@gmail.com>
2022-06-27 11:01:09 +0700

    feat: add region bar content-management

--- modules/Pages/content-management/content/RegionViews/index.js
+++ modules/Pages/content-management/content/RegionViews/index.js
@@ -3,13 +3,28 @@ import CmtProgressBar from "../../../../../@coremat/CmtProgressBar";
 import React from "react";
 import ContentDataCard from "../ContentDataCard";
 
-const RegionViews = ({}) =>{
-    const value = 100;
-    const label = 'Indonesia';
+const RegionViews = ({regions}) =>{
+    // const value = 1;
+    // const label = 'Indonesia';
     const color = '#AB22AF';
     return(
         <ContentDataCard title={'Recently Region Viewers'} contentType={'HyppeVid'}  content={<Box width={1} mb={{xs: 3, sm: 6}}>
-            <CmtProgressBar
+            {regions?.map((region) => {
+                return (
+                    <CmtProgressBar
+                label={<Box mb={-1}>{region?._id}</Box>}
+                labelPos="top-left"
+                value={region?.totalpost}
+                renderValue={(value) => {
+                    return `${value}%`;
+                }}
+                containedColor={color}
+                thickness={7}
+                onlyContained
+            />
+                )
+            })}
+            {/* <CmtProgressBar
                 label={<Box mb={-1}>{label}</Box>}
                 labelPos="top-left"
                 value={value}
@@ -19,7 +34,7 @@ const RegionViews = ({}) =>{
                 containedColor={color}
                 thickness={7}
                 onlyContained
-            />
+            /> */}
         </Box>}/>
     )
 }
