40aa5ecb8a1d69518f64807a423fa962abaa6f90
vickyfto <vickyfitrio19@gmail.com>
2022-07-05 11:50:58 +0700

    feat: slicing UI finish activation

---
+++ modules/Pages/finish-activation/index.js
@@ -0,0 +1,34 @@
+import CmtCard from '@coremat/CmtCard';
+import GridContainer from '@jumbo/components/GridContainer';
+import { Box, Button, Grid, Typography } from '@material-ui/core';
+
+const FinishActivation = () => {
+  return (
+    <>
+      <CmtCard style={{ minHeight: '500px' }}>
+        <center style={{ marginTop: '7%' }}>
+          <img src="./images/kotak.png" width="100" height="100" style={{ display: 'block' }} />
+          <Typography component="div" variant="h1" style={{ margin: '10px 0 ' }}>
+            Selesaikan Aktivasi Anda
+          </Typography>
+          <Typography component="div" variant="h5" style={{ color: 'rgba(0, 0, 0, 0.6)' }}>
+            Selangkah lagi untuk mengaktifkan premium Anda, silakan klik tombol konfirmasi
+          </Typography>
+          <Button
+            variant="outlined"
+            style={{
+              background: 'rgb(170, 34, 175)',
+              color: '#FFFFFF',
+              border: 'none',
+              padding: '8px 20px',
+              borderRadius: '5px',
+              marginTop: '10px',
+            }}>
+            KONFIRMASI
+          </Button>
+        </center>
+      </CmtCard>
+    </>
+  );
+};
+export default FinishActivation;
