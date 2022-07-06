cc7235080478cbfdef46448c63f45a39bd33ea97
vickyfto <vickyfitrio19@gmail.com>
2022-07-06 17:23:11 +0700

    WIP: upgrade role user

--- modules/Pages/link-verification/index.js
+++ modules/Pages/link-verification/index.js
@@ -1,4 +1,24 @@
+import { useEffect } from 'react';
+import { useUpgradeUserMutation } from 'api/user/auth';
+// import { useAuth } from 'authentication';
+import { useRouter } from 'next/router';
+
 const LinkVerification = () => {
+  // const { authUser } = useAuth();
+  const router = useRouter();
+  const [upgradeUser, { isSuccess }] = useUpgradeUserMutation();
+
+  useEffect(() => {
+    // router.push(`link-verification?key=${authUser.email}`);
+    upgradeUser({ email: router.query.key, roles: 'ROLE_PREMIUM', status: 'FINISH' }).then((res) => {
+      if (res.data.status_user === 'FINISH') {
+        alert('akun anda sudah premium');
+        router.push('/');
+      } else {
+        alert('gagal upgrade premium');
+      }
+    });
+  }, []);
   return <div>Link LinkVerification</div>;
 };
 
