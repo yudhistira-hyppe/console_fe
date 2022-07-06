35d391614a7b996e6911b5fa6b23015957777123
vickyfto <vickyfitrio19@gmail.com>
2022-07-06 15:13:45 +0700

    fix: tabs content managemenet

--- modules/Pages/content-management/index.js
+++ modules/Pages/content-management/index.js
@@ -1,27 +1,18 @@
 /* eslint-disable react/jsx-key */
-import React, { useEffect, useState } from 'react';
-import PageHeader from '../../../@jumbo/components/PageComponents/PageHeader';
-import ContentDataCard from './ContentDataCard';
-import GridContainer from '../../../@jumbo/components/GridContainer';
-import { Box, Grid, makeStyles, Tab, Tabs } from '@material-ui/core';
-import RegionViews from './RegionViews';
-import { useUserContentsManagementQuery } from 'api/user/content/management';
-import { useAuth } from 'authentication';
-import SpinnerLoading from 'components/common/spinner';
-import { STREAM_URL } from 'authentication/auth-provider/config';
+import React, { useState } from 'react';
+import { makeStyles, Tab } from '@material-ui/core';
 import { TabContext, TabList, TabPanel } from '@material-ui/lab';
 import Analytic from './analytic';
 import Monetize from './monetize';
 import ContentList from './ContentList';
 import ContentManagement from './dashboard';
 
-const tabHeight = '40px'; // default: '48px'
 const useStyles = makeStyles((theme) => ({
   indicator: {
     // if you want to get it at the top
     // top: '0px',
-    maxWidth: '70px',
-    marginLeft: '4%',
+    // maxWidth: '70px',
+    // marginLeft: '4%',
   },
   tabsRoot: {
     // minHeight: tabHeight,
@@ -29,13 +20,15 @@ const useStyles = makeStyles((theme) => ({
     // border: '1px solid black',
   },
   tabRoot: {
-    minHeight: tabHeight,
-    height: tabHeight,
+    minHeight: '40px',
+    height: '40px',
+    maxWidth: '30px',
     textAlign: 'center',
     fontSize: '0.8em',
     fontWeight: '900',
-    margin: '0 -20px',
+    padding: '0 -20px',
     letterSpacing: '2px',
+    // border: '1px solid black',
   },
 }));
 
@@ -50,7 +43,6 @@ const Content = ({}) => {
 
   return (
     <>
-      {/* <PageHeader heading={'Content Management'} /> */}
       <TabContext value={value}>
         <TabList
           onChange={handleChange}
@@ -93,13 +85,13 @@ const Content = ({}) => {
           <ContentManagement />
         </TabPanel>
         <TabPanel value="2">
-          <Analytic />
+          <ContentList />
         </TabPanel>
         <TabPanel value="3">
-          <Monetize />
+          <Analytic />
         </TabPanel>
         <TabPanel value="4">
-          <ContentList />
+          <Monetize />
         </TabPanel>
       </TabContext>
     </>
