82f2856176d5f4d73864e262e17063fa4919c98c
vickyfto <vickyfitrio19@gmail.com>
2022-07-11 16:48:26 +0700

    feat: slicing UI welcome page business

---
+++ pages/home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import dynamic from 'next/dynamic';
+import PageLoader from '../@jumbo/components/PageComponents/PageLoader';
+
+const Home = dynamic(() => import('../modules/Pages/Home'), {
+  loading: () => <PageLoader />,
+});
+
+const HomePage = () => <Home />;
+
+export default HomePage;
