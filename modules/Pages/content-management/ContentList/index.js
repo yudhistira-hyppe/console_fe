c79bf994e5f9d66f27fb726991aff44d26df77ec
vickyfto <vickyfitrio19@gmail.com>
2022-07-15 02:20:21 +0700

    WIP: content management => monetize (this hard af)

--- modules/Pages/content-management/ContentList/index.js
+++ modules/Pages/content-management/ContentList/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import CmtCard from '../../../../@coremat/CmtCard';
 import CmtCardContent from '../../../../@coremat/CmtCard/CmtCardContent';
 import { fakeDb } from '../../../FakeDb/fake-db';
@@ -12,6 +12,9 @@ import TextField from '@material-ui/core/TextField';
 import IntlMessages from '../../../../@jumbo/utils/IntlMessages';
 import ContentTable from './ContentTable';
 import Pagination from '@material-ui/lab/Pagination';
+import { Stack } from '@mui/material';
+import { useRouter } from 'next/router';
+import { useUserContentsGroupQuery } from 'api/user/content/management';
 
 const actions = [
   {
@@ -32,39 +35,87 @@ const useStyles = makeStyles((theme) => ({
   cardContentRoot: {
     padding: '0 !important',
   },
-  titleRoot: {
-    letterSpacing: 0.15,
-  },
-  scrollbarRoot: {
-    height: 340,
-  },
-  badgeRoot: {
-    color: theme.palette.common.white,
-    borderRadius: 30,
-    fontSize: 12,
-    padding: '2px 10px',
-    display: 'inline-block',
-  },
-  buttonContainer: {
-    width: '30%',
-    display: 'flex',
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-  },
-  boxFiled: {
-    height: '36px !important',
-    fontFamily: 'Lato',
-    fontSize: '14px',
+  btnFilter: {
+    fontSize: '10px',
+    color: 'rgba(0, 0, 0, 0.38)',
   },
 }));
 
 const ContentList = () => {
   const { contentList } = fakeDb;
+  const router = useRouter();
   const numberPerPage = 5;
   const [nPage, setnPage] = useState(0);
   const [tableData, setTableData] = useState(contentList);
   const [page, setPage] = useState(1);
   const classes = useStyles();
+  // const [query, setQuery] = useState(['post']);
+  // const [key, setKey] = useState('');
+  // console.log('key:', key);
+
+  // const [payload, setPayload] = useState({
+  //   email: 'freeman27@getnada.com',
+  //   ownership: false,
+  //   monetesisasi: false,
+  //   archived: false,
+  //   buy: false,
+  //   // ini untuk filter
+  //   // startdate: '2022-01-11',
+  //   // enddate: '2022-01-11',
+  //   // postType: 'diary',
+  //   skip: 0,
+  //   limit: 10,
+  // });
+
+  const [btnLooping, setBtnLooping] = useState([
+    {
+      name: 'tes1',
+      id: '1',
+    },
+    {
+      name: 'tes2',
+      id: '2',
+    },
+    {
+      name: 'tes3',
+      id: '3',
+    },
+    {
+      name: 'tes4',
+      id: '4',
+    },
+    {
+      name: 'tes5',
+      id: '5',
+    },
+  ]);
+
+  const [state, setState] = useState(['tes1']);
+  console.log('state:', state);
+
+  // const aish = ['tes2', 'tes3', 'tes4', 'tes5'];
+
+  const clickedButton = (name) => {
+    if (name === 'tes1') return setState(['tes1']);
+
+    if (name !== 'tes1') {
+      if (state.includes('tes1')) {
+        setState((prev) => {
+          const removeTes1 = prev.filter((e) => e !== 'tes1');
+          console.log('removeTes1:', removeTes1);
+          return [...removeTes1];
+        });
+      }
+      setState((prev) => [...prev, name]);
+    }
+
+    if (state.includes(name)) {
+      setState((prev) => {
+        const findAndRemoveMatchValue = prev.filter((e) => e !== name);
+        return [...findAndRemoveMatchValue];
+      });
+    }
+  };
 
   useEffect(() => {
     var quotient = Math.floor(contentList.length / numberPerPage);
@@ -81,50 +132,30 @@ const ContentList = () => {
 
   return (
     <div>
-      <PageHeader heading={'Content'} />
-      <div className="flex flex-row w-full -mt-5">
+      {/* <PageHeader heading={'Content'} /> */}
+      <div className="flex flex-row w-full mt-3">
         <div className="flex flex-row col-8 align-items-center">
-          <TextField
-            size={'small'}
-            className="mr-2"
-            label={'Post'}
-            // placeholder={'Post'}
-            margin="normal"
-            variant="outlined"
-          />
-          <TextField
-            size={'small'}
-            className="mr-2"
-            label={'Ownership'}
-            // placeholder={'Ownership'}
-            margin="normal"
-            variant="outlined"
-          />
-          <TextField
-            size={'small'}
-            className="mr-2"
-            label={'Monetisasi'}
-            // placeholder={'Monetisasi'}
-            margin="normal"
-            variant="outlined"
-          />
-          <TextField
-            size={'small'}
-            className="mr-2"
-            label={'Beli'}
-            // placeholder={'Beli'}
-            margin="normal"
-            variant="outlined"
-          />
-          <TextField
-            size={'small'}
-            className="mr-2"
-            label={'Archived'}
-            // placeholder={'Archived'}
-            margin="normal"
-            variant="outlined"
-          />
+          <Stack direction="row" spacing={1}>
+            {btnLooping?.map((btn) => {
+              return (
+                <Button
+                  name={`${btn.name}`}
+                  variant="outlined"
+                  size="small"
+                  className={classes.btnFilter}
+                  style={
+                    state.includes(btn.name)
+                      ? { backgroundColor: ' rgba(171, 34, 175, 0.12)', color: 'rgba(171, 34, 175, 1)' }
+                      : null
+                  }
+                  onClick={() => clickedButton(btn.name)}>
+                  {btn.name}
+                </Button>
+              );
+            })}
+          </Stack>
         </div>
+
         <div className="flex flex-row-reverse col-4 align-items-center">
           <FormControl size={'small'} className="mr-2 mt-2" variant={'outlined'} fullWidth>
             <InputLabel id="fitur-select-label">Semua Fitur</InputLabel>
@@ -136,7 +167,7 @@ const ContentList = () => {
           </FormControl>
           <FormControl size={'small'} className="mr-2 mt-2" variant={'outlined'} fullWidth>
             <InputLabel id="content-select-label"> Waktu Post</InputLabel>
-            <Select labelId="content-select-label" id="fitur-simple-select" label=" di post">
+            <Select labelId="content-select-label" id="fitur-simple-select" label="di post">
               <MenuItem value={10}>Ten</MenuItem>
               <MenuItem value={20}>Twenty</MenuItem>
               <MenuItem value={30}>Thirty</MenuItem>
