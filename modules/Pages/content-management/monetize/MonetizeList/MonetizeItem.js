4e3f4017d58c4f24279a5c381d6eba3c783581f0
vickyfto <vickyfitrio19@gmail.com>
2022-07-15 17:36:18 +0700

    intergrasi: api contents => pembelian konten

--- modules/Pages/content-management/monetize/MonetizeList/MonetizeItem.js
+++ modules/Pages/content-management/monetize/MonetizeList/MonetizeItem.js
@@ -3,97 +3,108 @@ import TableRow from '@material-ui/core/TableRow';
 import TableCell from '@material-ui/core/TableCell';
 import clsx from 'clsx';
 import { alpha, makeStyles } from '@material-ui/core/styles';
-import {IconButton, Switch, Tooltip} from '@material-ui/core';
+import { IconButton, Switch, Tooltip } from '@material-ui/core';
 import Box from '@material-ui/core/Box';
-import {RemoveRedEye, Delete, Edit, TrendingUp} from "@material-ui/icons";
-import CmtImage from "../../../../../@coremat/CmtImage";
+import { RemoveRedEye, Delete, Edit, TrendingUp } from '@material-ui/icons';
+import CmtImage from '../../../../../@coremat/CmtImage';
+import { useAuth } from 'authentication';
+import { STREAM_URL } from 'authentication/auth-provider/config';
 
 const useStyles = makeStyles((theme) => ({
-    tableRowRoot: {
-        position: 'relative',
-        transition: 'all .2s',
-        borderTop: `solid 1px ${theme.palette.borderColor.main}`,
-        '&:last-child': {
-            borderBottom: `solid 1px ${theme.palette.borderColor.main}`,
-        },
+  tableRowRoot: {
+    position: 'relative',
+    transition: 'all .2s',
+    borderTop: `solid 1px ${theme.palette.borderColor.main}`,
+    '&:last-child': {
+      borderBottom: `solid 1px ${theme.palette.borderColor.main}`,
     },
-    tableCellRoot: {
-        padding: 16,
-        fontSize: 14,
-        fontFamily:'Lato',
-        letterSpacing: 0.25,
-        color: 'rgba(0, 0, 0, 0.6)',
-        borderBottom: '0 none',
-        position: 'relative',
-        '&:first-child': {
-            paddingLeft: 24,
-        },
-        '&.success': {
-            color: theme.palette.success.main,
-        },
-        '& .Cmt-media-object': {
-            alignItems: 'center',
-        },
+  },
+  tableCellRoot: {
+    padding: 16,
+    fontSize: 14,
+    fontFamily: 'Lato',
+    letterSpacing: 0.25,
+    color: 'rgba(0, 0, 0, 0.6)',
+    borderBottom: '0 none',
+    position: 'relative',
+    '&:first-child': {
+      paddingLeft: 24,
     },
-    badgeRoot: {
-        color: theme.palette.common.white,
-        borderRadius: 30,
-        fontSize: 12,
-        padding: '2px 10px',
-        display: 'inline-block',
-        width: '80px'
+    '&.success': {
+      color: theme.palette.success.main,
     },
-    subInfo: {
-        fontFamily: 'Lato',
-        fontSize: '12px',
-        lineHeight: '16px',
-        letterSpacing: '0.25px',
-        color: 'rgba(0, 0, 0, 0.6)'
-    }
+    '& .Cmt-media-object': {
+      alignItems: 'center',
+    },
+  },
+  badgeRoot: {
+    color: theme.palette.common.white,
+    borderRadius: 30,
+    fontSize: 12,
+    padding: '2px 10px',
+    display: 'inline-block',
+    width: '80px',
+  },
+  subInfo: {
+    fontFamily: 'Lato',
+    fontSize: '12px',
+    lineHeight: '16px',
+    letterSpacing: '0.25px',
+    color: 'rgba(0, 0, 0, 0.6)',
+  },
 }));
 
 function getBgColor(status) {
-    const color = {
-        sent: '#9BE7FD',
-        receive: '#D7F5B1'
-    };
-    return color[status];
+  const color = {
+    sent: '#9BE7FD',
+    receive: '#D7F5B1',
+  };
+  return color[status];
 }
 
-const MonetizeItem = ({row }) => {
-    const classes = useStyles();
-    const label = { inputProps: { 'aria-label': 'Switch Monetize' } };
-    return (
-        <TableRow className={classes.tableRowRoot}>
-            <TableCell className={classes.tableCellRoot}>
-                <div className='flex flex-row align-items-center align-content-center'>
-                    <CmtImage alt={row.name} src={row.contentImage}/>
-                    <div className='ml-1'>{row.contentName}</div>
-                </div>
-            </TableCell>
-            <TableCell className={classes.tableCellRoot}>
-                {row.date}
-            </TableCell>
-            <TableCell className={classes.tableCellRoot}>
-                {row.type}
-            </TableCell>
-            <TableCell className={classes.tableCellRoot}>
-                {row.contentId}
-            </TableCell>
-            <TableCell className={classes.tableCellRoot}>
-                {row.certNumber}
-            </TableCell>
-            <TableCell className={classes.tableCellRoot}>
-                <div className='flex flex-row align-items-center'>
-                    <div>Rp. 1.500.000</div>
-                    <Edit/>
-                </div>
-            </TableCell>
-            <TableCell className={classes.tableCellRoot}>
-                <TrendingUp/>
-            </TableCell>
-        </TableRow>
-    );
+const MonetizeItem = ({ row }) => {
+  const { authUser } = useAuth();
+  const classes = useStyles();
+  const label = { inputProps: { 'aria-label': 'Switch Monetize' } };
+
+  const getMediaUri = () => {
+    const authToken = `?x-auth-token=${authUser.token}&x-auth-user=${authUser.email}`;
+    const mediaURI = '/thumb/' + row?.postID;
+
+    return `${STREAM_URL}${mediaURI}${authToken}`;
+  };
+  return (
+    <TableRow className={classes.tableRowRoot}>
+      <TableCell className={classes.tableCellRoot}>
+        <div className="flex flex-row align-items-center align-content-center">
+          {/* <CmtImage alt={row.name} src={row.contentImage} /> */}
+          <div
+            style={{
+              width: '100px',
+              height: '100px',
+              backgroundImage: `url('${getMediaUri()}')`,
+              backgroundRepeat: 'no-repeat',
+              backgroundPosition: 'center',
+              backgroundSize: 'cover',
+            }}></div>
+          <div className="ml-1">{row.title}</div>
+        </div>
+      </TableCell>
+      <TableCell className={classes.tableCellRoot}>{row?.createdAt}</TableCell>
+      <TableCell className={classes.tableCellRoot}>{row?.postType}</TableCell>
+      <TableCell className={classes.tableCellRoot}>{row?._id}</TableCell>
+      <TableCell className={classes.tableCellRoot}>{row?._id}</TableCell>
+      <TableCell className={classes.tableCellRoot}>
+        <div className="flex flex-row align-items-center">
+          <div>{row?.saleAmount}</div>
+          <Edit />
+        </div>
+      </TableCell>
+      <TableCell className={classes.tableCellRoot}>
+        <TrendingUp />
+      </TableCell>
+    </TableRow>
+  );
 };
 
 export default MonetizeItem;
--- modules/Pages/content-management/monetize/index.js
+++ modules/Pages/content-management/monetize/index.js
@@ -1,77 +1,146 @@
-import React, {useState} from "react";
-import {PageHeader} from "../../../../@jumbo/components/PageComponents";
-import {FormControl, Grid, InputLabel, MenuItem, Select} from "@material-ui/core";
-import {makeStyles} from "@material-ui/core/styles";
-import CmtCard from "../../../../@coremat/CmtCard";
-import CmtCardContent from "../../../../@coremat/CmtCard/CmtCardContent";
-import DetailsCard from "../details/DetailsCard";
-import PerfectScrollbar from "react-perfect-scrollbar";
-import MonetizeList from "./MonetizeList/MonetizeList";
-import {fakeDb} from "../../../FakeDb/fake-db";
-import MonetizeTabs from "./MonetizeTabs";
+import React, { useEffect, useState } from 'react';
+import { PageHeader } from '../../../../@jumbo/components/PageComponents';
+import { FormControl, Grid, InputLabel, MenuItem, Select } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import CmtCard from '../../../../@coremat/CmtCard';
+import CmtCardContent from '../../../../@coremat/CmtCard/CmtCardContent';
+import DetailsCard from '../details/DetailsCard';
+import PerfectScrollbar from 'react-perfect-scrollbar';
+import MonetizeList from './MonetizeList/MonetizeList';
+import { fakeDb } from '../../../FakeDb/fake-db';
+import MonetizeTabs from './MonetizeTabs';
+import { useUserContentMonetizeQuery } from 'api/user/content/management';
+import { useAuth } from 'authentication';
+import { STREAM_URL } from 'authentication/auth-provider/config';
 
 const useStyles = makeStyles((theme) => ({
-    scrollbarRoot: {
-        height: 320,
-        '& .CmtList-EmptyResult': {
-            backgroundColor: 'transparent',
-            border: '0 none',
-        },
+  scrollbarRoot: {
+    height: 320,
+    '& .CmtList-EmptyResult': {
+      backgroundColor: 'transparent',
+      border: '0 none',
     },
-}))
+  },
+}));
 
 const Montetize = ({}) => {
-    const {contentList} = fakeDb;
-    const classes = useStyles();
-    const [tabValue, setTabValue] = useState("monetize_content");
-    const onChangeTab = (value) => {
-        setTabValue(value);
-    };
-    return (
-        <div>
-            <PageHeader heading={'Monetisasi'}
-                        children={<div className='flex flex-row-reverse col-4 align-items-center'>
-                            <FormControl size={"small"} className='mr-2 mt-2' variant={"outlined"} fullWidth>
-                                <InputLabel id="fitur-select-label">Semua Fitur</InputLabel>
-                                <Select
-                                    labelId="fitur-select-label"
-                                    id="fitur-simple-select"
-                                    label="Semua Fitur"
-                                >
-                                    <MenuItem value={10}>Ten</MenuItem>
-                                    <MenuItem value={20}>Twenty</MenuItem>
-                                    <MenuItem value={30}>Thirty</MenuItem>
-                                </Select>
-                            </FormControl>
-                            <FormControl size={"small"} className='mr-2 mt-2' variant={"outlined"} fullWidth>
-                                <InputLabel id="content-select-label">Kontent di post</InputLabel>
-                                <Select
-                                    labelId="content-select-label"
-                                    id="fitur-simple-select"
-                                    label="Kontent di post"
-                                >
-                                    <MenuItem value={10}>Ten</MenuItem>
-                                    <MenuItem value={20}>Twenty</MenuItem>
-                                    <MenuItem value={30}>Thirty</MenuItem>
-                                </Select>
-                            </FormControl>
-                        </div>}/>
-            <CmtCard>
-                <CmtCardContent>
-                    <MonetizeTabs tabValue={tabValue} onChangeTab={onChangeTab}/>
-                    <PerfectScrollbar className={classes.scrollbarRoot}>
-                        <MonetizeList tableData={contentList}></MonetizeList>
-                    </PerfectScrollbar>
-                </CmtCardContent>
-            </CmtCard>
-            <div className='mt-6 col-6 p-0'>
-                <DetailsCard title={'Monetisasi Konten Terbaru'}
-                             contentTitle={'Hari ini bersama keluarga tersayang liburan ke pa... '} likes={233}
-                             comments={233} category={'Entertaint, Hobby'} ownership={'23EY82KD02L'} views={233}
-                             date={'Jun 26, 2020'} contentType={'HyppeVid'}/>
-            </div>
-        </div>
-    )
-}
+  const { contentList } = fakeDb;
+  const { authUser } = useAuth();
+  const classes = useStyles();
+  const [tabValue, setTabValue] = useState('monetize_content');
+  console.log('tabValue:', tabValue);
+
+  const [payload, setPayload] = useState();
+
+  const onChangeTab = (value) => {
+    setTabValue(value);
+  };
+
+  const payloadLastContent = {
+    email: authUser.email,
+    buy: false,
+    monetize: false,
+    lastmonetize: true,
+    // postType: 'diary',
+    skip: 0,
+    limit: 10,
+  };
+  const { data: lastContentMonetize } = useUserContentMonetizeQuery(payloadLastContent);
+
+  const { data: contentMonetize } = useUserContentMonetizeQuery(payload);
+  console.log('contentMonetize:', contentMonetize);
+
+  useEffect(() => {
+    switch (tabValue) {
+      case 'monetize_content':
+        return setPayload({
+          email: authUser.email,
+          buy: false,
+          monetize: true,
+          lastmonetize: false,
+          // postType: 'diary',
+          skip: 0,
+          limit: 10,
+        });
+      case 'buy_content':
+        return setPayload({
+          email: authUser.email,
+          buy: true,
+          monetize: false,
+          lastmonetize: false,
+          // postType: 'diary',
+          skip: 0,
+          limit: 10,
+        });
+      default:
+        console.log(`masuk default`);
+    }
+  }, [tabValue]);
+
+  const formatDate = () => {
+    return new Date(lastContentMonetize?.data[0]?.createdAt.split(' ')[0]).toLocaleString('en-us', {
+      month: 'short',
+      year: 'numeric',
+      day: 'numeric',
+    });
+  };
+
+  const getMediaUri = () => {
+    const authToken = `?x-auth-token=${authUser.token}&x-auth-user=${authUser.email}`;
+    const mediaURI = '/thumb/' + lastContentMonetize?.data[0]?.postID;
+
+    return `${STREAM_URL}${mediaURI}${authToken}`;
+  };
+
+  return (
+    <div>
+      <PageHeader
+        heading={'Monetisasi'}
+        children={
+          <div className="flex flex-row-reverse col-4 align-items-center">
+            <FormControl size={'small'} className="mr-2 mt-2" variant={'outlined'} fullWidth>
+              <InputLabel id="fitur-select-label">Semua Fitur</InputLabel>
+              <Select labelId="fitur-select-label" id="fitur-simple-select" label="Semua Fitur">
+                <MenuItem value={10}>Ten</MenuItem>
+                <MenuItem value={20}>Twenty</MenuItem>
+                <MenuItem value={30}>Thirty</MenuItem>
+              </Select>
+            </FormControl>
+            <FormControl size={'small'} className="mr-2 mt-2" variant={'outlined'} fullWidth>
+              <InputLabel id="content-select-label">Kontent di post</InputLabel>
+              <Select labelId="content-select-label" id="fitur-simple-select" label="Kontent di post">
+                <MenuItem value={10}>Ten</MenuItem>
+                <MenuItem value={20}>Twenty</MenuItem>
+                <MenuItem value={30}>Thirty</MenuItem>
+              </Select>
+            </FormControl>
+          </div>
+        }
+      />
+      <CmtCard>
+        <CmtCardContent>
+          <MonetizeTabs tabValue={tabValue} onChangeTab={onChangeTab} />
+          <PerfectScrollbar className={classes.scrollbarRoot}>
+            <MonetizeList tableData={contentMonetize?.data}></MonetizeList>
+          </PerfectScrollbar>
+        </CmtCardContent>
+      </CmtCard>
+      <div className="mt-6 col-6 p-0">
+        <DetailsCard
+          title={lastContentMonetize?.data[0]?.title}
+          contentTitle={lastContentMonetize?.data[0]?.description}
+          likes={lastContentMonetize?.data[0]?.likes}
+          comments={lastContentMonetize?.data[0]?.comments}
+          category={lastContentMonetize?.data[0]?.postType}
+          ownership={lastContentMonetize?.data[0]?._id}
+          views={lastContentMonetize?.data[0]?.views}
+          date={formatDate()}
+          contentType={lastContentMonetize?.data[0]?.postType}
+          image={getMediaUri()}
+        />
+      </div>
+    </div>
+  );
+};
 
 export default Montetize;
