380783645974b271578e541fb0bfcd6a6f5d8a58
vickyfto <vickyfitrio19@gmail.com>
2022-07-25 17:51:59 +0700

    feat: fix responsive && bug, etccccc

--- api/user/content/index.js
+++ api/user/content/index.js
@@ -62,7 +62,7 @@ export const {
   useUserContentsLatestQuery,
   useUserContentsPopularQuery,
   useUserContentsMonetizeQuery,
-  useUserContentEventQuery,
+  ,
   useUserContentTimeQuery,
   useUserContentDetailsQuery,
 } = contentAPI;
