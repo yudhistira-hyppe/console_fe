399e63f1daf641ace49297ba2f974d7ed7719131
vickyfto <vickyfitrio19@gmail.com>
2022-07-28 11:38:43 +0700

    fix deploy

--- pages/ads/paymentSuccess.js
+++ pages/ads/paymentSuccess.js
@@ -2,7 +2,6 @@ import React from 'react';
 import dynamic from 'next/dynamic';
 import PageLoader from '../../@jumbo/components/PageComponents/PageLoader';
 import SecurePage from '../../authentication/auth-page-wrappers/SecurePage';
-import PaymentSuccessful from '../../modules/Pages/ads/CreateCampaign/PaymentSuccessful';
 
 const PaymentSuccessful = dynamic(() => import('../../modules/Pages/ads/CreateCampaign/PaymentSuccessful'), {
   loading: () => <PageLoader />,
