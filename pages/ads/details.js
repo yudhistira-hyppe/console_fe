6123860382a4ce2400c048c52ea70a138d096a4a
vickyfto <vickyfitrio19@gmail.com>
2022-07-28 11:36:03 +0700

    feat: imprort ads components

--- pages/ads/details.js
+++ pages/ads/details.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import dynamic from 'next/dynamic';
-import PageLoader from "../../@jumbo/components/PageComponents/PageLoader";
-import SecurePage from "../../authentication/auth-page-wrappers/SecurePage";
-import CampaignDetail from "../../modules/Pages/ads/CampaignDetail";
+import PageLoader from '../../@jumbo/components/PageComponents/PageLoader';
+import SecurePage from '../../authentication/auth-page-wrappers/SecurePage';
 
-const AdsCreateModule = dynamic(() => import('../../modules/Pages/ads/CampaignDetail'), {
-    loading: () => <PageLoader/>,
+const CampaignDetail = dynamic(() => import('../../modules/Pages/ads/CampaignDetail'), {
+  loading: () => <PageLoader />,
 });
 
 const CreateCampaign = () => (
-    <SecurePage>
-        <CampaignDetail/>
-    </SecurePage>
+  <SecurePage>
+    <CampaignDetail />
+  </SecurePage>
 );
 
 export default CreateCampaign;
