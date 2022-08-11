4af6ae36b69e056c97e5f6d161c204e5d502dae1
vickyfto <vickyfitrio19@gmail.com>
2022-08-11 16:05:28 +0700

    feat: refactor import api

--- api/user/index.js
+++ api/user/index.js
@@ -1,4 +1,21 @@
 import { authApi } from './auth';
 import { userApi } from './user';
+import { accountBalancesAPI } from './accountBalances';
+import { commentAPI } from './comment';
+import { contentAPI } from './content';
+import { userFriendAPI } from './friend';
+import { insightAPI } from './insight';
+import { notificationAPI } from './notification';
+import { contentManagementAPI } from './content/management';
 
-export { authApi, userApi };
+export {
+  authApi,
+  userApi,
+  accountBalancesAPI,
+  commentAPI,
+  contentAPI,
+  userFriendAPI,
+  insightAPI,
+  notificationAPI,
+  contentManagementAPI,
+};
--- redux/store/index.js
+++ redux/store/index.js
@@ -1,15 +1,18 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { createWrapper } from 'next-redux-wrapper';
 import reducers from '../reducers';
-import { authApi, userApi } from 'api/user';
+import {
+  authApi,
+  userApi,
+  accountBalancesAPI,
+  commentAPI,
+  userFriendAPI,
+  contentAPI,
+  insightAPI,
+  notificationAPI,
+  contentManagementAPI,
+} from 'api/user';
 import { dashboardApi, engagementApi, faqAndInfoApi, announcementApi, ticketApi, getUserHyppe, group } from 'api/console';
-import { contentAPI } from 'api/user/content';
-import { notificationAPI } from 'api/user/notification';
-import { commentAPI } from 'api/user/comment';
-import { insightAPI } from 'api/user/insight';
-import { contentManagementAPI } from 'api/user/content/management';
-import { accountBalancesAPI } from 'api/user/accountBalances';
-import { userFriendAPI } from 'api/user/friend';
 
 const initStore = () => {
   const store = configureStore({
