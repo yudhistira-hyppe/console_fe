b3da2479e8084cab1f0c0ba03a088c0a52605aa8
vickyfto <vickyfitrio19@gmail.com>
2022-08-15 17:45:07 +0700

    feat: intergrasi userHyppe and change input to select

--- modules/Pages/console/anggota/addPosition/index.js
+++ modules/Pages/console/anggota/addPosition/index.js
@@ -3,11 +3,22 @@ import TreeView from '@mui/lab/TreeView';
 // import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 // import ChevronRightIcon from '@mui/icons-material/ChevronRight';
 import TreeItem from '@mui/lab/TreeItem';
-import { Button, Checkbox, Divider, FormControlLabel, makeStyles, Box, Typography, TextField } from '@material-ui/core';
+import {
+  Button,
+  Checkbox,
+  Divider,
+  FormControlLabel,
+  makeStyles,
+  Box,
+  Typography,
+  TextField,
+  MenuItem,
+} from '@material-ui/core';
 import { useGetModuleQuery } from 'api/console/module';
 import { Stack } from '@mui/system';
 import PageContainer from '@jumbo/components/PageComponents/layouts/PageContainer';
 import { useRouter } from 'next/router';
+import { useGetAnggotaQuery } from 'api/console/getUserHyppe';
 
 const useStyles = makeStyles((theme) => ({
   checkbox: {
@@ -109,6 +120,39 @@ const RichObjectTreeView = () => {
     </TreeItem>
   );
 
+  const currencies = [
+    {
+      value: 'USD',
+      label: '$',
+    },
+    {
+      value: 'EUR',
+      label: '€',
+    },
+    {
+      value: 'BTC',
+      label: '฿',
+    },
+    {
+      value: 'JPY',
+      label: '¥',
+    },
+  ];
+
+  const [userSelect, setUserSelect] = React.useState('');
+
+  const handleChange = (event) => {
+    setUserSelect(event.target.value);
+  };
+
+  const payload = {
+    skip: 0,
+    limit: 100,
+  };
+
+  const { data: userHyppe } = useGetAnggotaQuery(payload);
+  console.log('userHyppe:', userHyppe);
+
   return (
     <>
       <Stack direction="row" justifyContent="space-between" alignItems="flex-start">
@@ -126,12 +170,18 @@ const RichObjectTreeView = () => {
         <Box sx={{ width: 500 }}>
           <TextField
             fullWidth
-            size="small"
-            id="outlined-basic"
-            label="Jabatan"
-            variant="outlined"
-            style={{ background: '#FFFFFF' }}
-          />
+            id="outlined-select-currency"
+            select
+            label="Select"
+            value={userSelect}
+            onChange={handleChange}
+            helperText="Please select user">
+            {userHyppe?.data?.map((item) => (
+              <MenuItem key={item?._id} value={item?._id}>
+                {item?.email}
+              </MenuItem>
+            ))}
+          </TextField>
         </Box>
         <Box color="rgba(0, 0, 0, 0.3)" sx={{ width: 400 }}>
           <span>Contoh : Customer Care / Staff atau Customer Care / Manager atau Customer Care / Head </span>
