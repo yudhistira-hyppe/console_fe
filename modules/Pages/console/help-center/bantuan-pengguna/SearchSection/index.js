8ef3b13eae83a18fb46909645820f7131debddf6
ardihp <ardihp8@gmail.com>
2022-09-27 13:47:18 +0700

    feat(help-center): integrate sumber ticket & kategori ticket & filter params index pengguna

--- modules/Pages/console/help-center/bantuan-pengguna/SearchSection/index.js
+++ modules/Pages/console/help-center/bantuan-pengguna/SearchSection/index.js
@@ -11,24 +11,33 @@ import { Stack } from '@mui/material';
 import { DateRangePicker } from '@mui/x-date-pickers-pro/DateRangePicker';
 import { AdapterDayjs } from '@mui/x-date-pickers-pro/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers-pro';
+import { debounce } from 'lodash';
+import { useGetCategoryTicketsQuery, useGetSumberTicketsQuery } from 'api/console/helpCenter/bantuan-pengguna';
 
-const SearchSection = () => {
+const SearchSection = ({ handleChange }) => {
   const classes = useStyles();
   const [week, setWeek] = React.useState(null);
   const [value, setValue] = React.useState([null, null]);
   function getWeeksAfter(date, amount) {
     return date && amount ? date.add(amount, 'week') : undefined;
   }
+  const { data: listSumber } = useGetSumberTicketsQuery();
+  const { data: listCategory } = useGetCategoryTicketsQuery();
+  const handleSearch = debounce((query) => handleChange('search', query), 500);
 
   return (
     <>
-      <Box className={classes.inBuildAppCard} p={5}>
+      <Box className={classes.inBuildAppCard} p={5} style={{ maxWidth: 250 }}>
         <Accordion elevation={0} defaultExpanded>
           <AccordionSummary expandIcon={<ExpandMoreIcon />} style={{ padding: '0px' }}>
-            <Typography style={{ fontSize: '13px' }}>Cari Nomor Tiket / Judul Masalah</Typography>
+            <Typography style={{ fontSize: '13px' }}>Pencarian Tiket</Typography>
           </AccordionSummary>
           <AccordionDetails style={{ padding: '0px' }}>
-            <TextField style={{ width: '100%' }} placeholder="Cari" />
+            <TextField
+              style={{ width: '100%' }}
+              placeholder="Cari No. Tiket / Judul Permasalahan"
+              onChange={(e) => handleSearch(e.target.value)}
+            />
           </AccordionDetails>
         </Accordion>
 
@@ -41,41 +50,45 @@ const SearchSection = () => {
               <Chip
                 clickable
                 onClick={() => {
+                  handleChange('ticket_date', 7);
                   setWeek(1), setValue([null, null]);
                 }}
                 label="7 Hari"
                 size="small"
-                style={{ width: '30%' }}
+                style={{ width: '35%' }}
                 variant={week == 1 ? 'default' : 'outlined'}
               />
               <Chip
                 label="14 Hari"
                 clickable
                 onClick={() => {
+                  handleChange('ticket_date', 14);
                   setWeek(2), setValue([null, null]);
                 }}
                 size="small"
-                style={{ width: '30%' }}
+                style={{ width: '35%' }}
                 variant={week === 2 ? 'default' : 'outlined'}
               />
               <Chip
                 label="1 Bulan"
                 clickable
                 onClick={() => {
+                  handleChange('ticket_date', 30);
                   setWeek(4), setValue([null, null]);
                 }}
                 size="small"
-                style={{ width: '30%' }}
+                style={{ width: '35%' }}
                 variant={week === 4 ? 'default' : 'outlined'}
               />
               <Chip
                 label="3 Bulan"
                 clickable
                 onClick={() => {
+                  handleChange('ticket_date', 90);
                   setWeek(12), setValue([null, null]);
                 }}
                 size="small"
-                style={{ width: '30%' }}
+                style={{ width: '35%' }}
                 variant={week === 12 ? 'default' : 'outlined'}
               />
             </Stack>
@@ -85,6 +98,7 @@ const SearchSection = () => {
                 value={value}
                 maxDate={getWeeksAfter(value[0], week)}
                 onChange={(newValue) => {
+                  handleChange('ticket_range', [newValue[0]?.format('YYYY-MM-DD'), newValue[1]?.format('YYYY-MM-DD')]);
                   setValue(newValue);
                 }}
                 renderInput={(startProps, endProps) => (
@@ -106,8 +120,13 @@ const SearchSection = () => {
           </AccordionSummary>
           <AccordionDetails style={{ padding: 0 }}>
             <FormGroup>
-              <FormControlLabel label={'Formulir'} control={<Checkbox defaultChecked={false} />} />
-              <FormControlLabel label={'Hyppe'} control={<Checkbox defaultChecked={false} />} />
+              {listSumber?.data?.map((item, key) => (
+                <FormControlLabel
+                  key={key}
+                  label={item?.sourceName}
+                  control={<Checkbox defaultChecked={false} onClick={() => handleChange('sumber', item?._id)} />}
+                />
+              ))}
             </FormGroup>
           </AccordionDetails>
         </Accordion>
@@ -118,10 +137,13 @@ const SearchSection = () => {
           </AccordionSummary>
           <AccordionDetails style={{ padding: 0 }}>
             <FormGroup>
-              <FormControlLabel label={'Akun & Verifikasi'} control={<Checkbox defaultChecked={false} />} />
-              <FormControlLabel label={'Konten'} control={<Checkbox defaultChecked={false} />} />
-              <FormControlLabel label={'Transaksi'} control={<Checkbox defaultChecked={false} />} />
-              <FormControlLabel label={'Kepemilikan'} control={<Checkbox defaultChecked={false} />} />
+              {listCategory?.data?.map((item, key) => (
+                <FormControlLabel
+                  key={key}
+                  label={item?.nameCategory}
+                  control={<Checkbox defaultChecked={false} onClick={() => handleChange('category', item?._id)} />}
+                />
+              ))}
             </FormGroup>
           </AccordionDetails>
         </Accordion>
@@ -132,10 +154,18 @@ const SearchSection = () => {
           </AccordionSummary>
           <AccordionDetails style={{ padding: 0 }}>
             <FormGroup>
-              <FormControlLabel label={'Baru'} control={<Checkbox defaultChecked={false} />} />
-              <FormControlLabel label={'Dalam Proses'} control={<Checkbox defaultChecked={false} />} />
-              <FormControlLabel label={'Selesai'} control={<Checkbox defaultChecked={false} />} />
-              <FormControlLabel label={'Tidak Selesai'} control={<Checkbox defaultChecked={false} />} />
+              <FormControlLabel
+                label={'Baru'}
+                control={<Checkbox defaultChecked={false} onClick={() => handleChange('status', 'new')} />}
+              />
+              <FormControlLabel
+                label={'Dalam Proses'}
+                control={<Checkbox defaultChecked={false} onClick={() => handleChange('status', 'onprogress')} />}
+              />
+              <FormControlLabel
+                label={'Selesai'}
+                control={<Checkbox defaultChecked={false} onClick={() => handleChange('status', 'close')} />}
+              />
             </FormGroup>
           </AccordionDetails>
         </Accordion>
