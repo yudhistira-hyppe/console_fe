87c7276c4fe0bd7a6dee426aaca7bdd59a51c672
ardihp <ardihp8@gmail.com>
2022-09-28 17:43:04 +0700

    feat(help-center): setup query function for detail ticket

--- api/console/helpCenter/bantuan-pengguna.js
+++ api/console/helpCenter/bantuan-pengguna.js
@@ -12,20 +12,63 @@ export const bantuanPenggunaApi = createApi({
         method: 'POST',
         body: params,
       }),
+      providesTags: ['TicketList'],
     }),
     getSumberTickets: build.query({
       query: () => ({
         url: '/sourcetickets/all',
         method: 'GET',
       }),
+      providesTags: ['TicketList'],
     }),
     getCategoryTickets: build.query({
       query: () => ({
         url: '/categorytickets/all',
         method: 'GET',
       }),
+      providesTags: ['TicketList'],
+    }),
+    getDetailTicket: build.query({
+      query: (params) => ({
+        url: '/usertickets/comment',
+        method: 'POST',
+        body: params,
+      }),
+      providesTags: ['TicketList'],
+    }),
+    updateDetailTicket: build.mutation({
+      query: ({ id, data }) => ({
+        url: `/usertickets/update/${id}`,
+        method: 'PUT',
+        body: data,
+      }),
+      invalidatesTags: ['TicketList'],
+    }),
+    replyTicket: build.mutation({
+      query: (data) => ({
+        url: '/usertickets/reply',
+        method: 'POST',
+        body: data,
+      }),
+      invalidatesTags: ['TicketList'],
+    }),
+    getLogHistoryDetailTicket: build.query({
+      query: (data) => ({
+        url: '/logtickets/history',
+        method: 'POST',
+        body: data,
+      }),
+      providesTags: ['TicketList'],
     }),
   }),
 });
 
-export const { useGetListTicketsQuery, useGetSumberTicketsQuery, useGetCategoryTicketsQuery } = bantuanPenggunaApi;
+export const {
+  useGetListTicketsQuery,
+  useGetSumberTicketsQuery,
+  useGetCategoryTicketsQuery,
+  useGetDetailTicketQuery,
+  useUpdateDetailTicketMutation,
+  useReplyTicketMutation,
+  useGetLogHistoryDetailTicketQuery,
+} = bantuanPenggunaApi;
