50e87f07de84b65eabf97fb7c46baecc3ae49efe
ardihp <ardihp8@gmail.com>
2022-09-29 16:21:31 +0700

    feat(monetize): slicing ui menu kepemilikan

--- modules/Pages/console/monetize/Kepemilikan/index.js
+++ modules/Pages/console/monetize/Kepemilikan/index.js
@@ -1,14 +1,22 @@
 import PageContainer from '@jumbo/components/PageComponents/layouts/PageContainer';
 import React from 'react';
-import { Typography } from '@mui/material';
+import GridContainer from '@jumbo/components/GridContainer';
+import { Grid } from '@material-ui/core';
+import SearchSection from '../components/KepemilikianSearchSection';
+import TableSection from '../components/KepemilikanTableSection';
 
 const MonetizeKepemilikanComponent = () => {
   return (
     <>
       <PageContainer>
-        <center>
-          <Typography color="#D0D0D0" fontWeight="bold" variant="h4">Coming Soon</Typography>
-        </center>
+        <GridContainer>
+          <Grid item xs={12} md={3} sm={3}>
+            <SearchSection />
+          </Grid>
+          <Grid item xs={12} md={9} sm={9}>
+            <TableSection order="desc" />
+          </Grid>
+        </GridContainer>
       </PageContainer>
     </>
   );
