10c6ea68ded755325465f83f2630749393eabd16
ardihp <ardihp8@gmail.com>
2022-11-14 17:58:02 +0700

    feat(help-center): page for permohonan premium

---
+++ modules/Pages/console/help-center/permohonan-premium/index.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import Head from 'next/head';
+import { Stack } from '@mui/material';
+import Breadcrumbs from '../bantuan-pengguna/BreadCrumb';
+import BackIconNav from '@material-ui/icons/ArrowBackIos';
+import PageContainer from '@jumbo/components/PageComponents/layouts/PageContainer';
+import { Typography } from '@material-ui/core';
+import router from 'next/router';
+
+const breadcrumbs = [
+  { label: 'Pusat Bantuan', link: '/help-center' },
+  { label: 'Permohonan Akun Premium', isActive: true },
+];
+
+const PermohonanPremium = () => {
+  return (
+    <>
+      <Head>
+        <title key="title">Hyppe-Console :: Permohonan Akun Premium</title>
+      </Head>
+      <Stack direction={'column'} spacing={2} mb={3}>
+        <Breadcrumbs breadcrumbs={breadcrumbs} />
+        <Stack
+          direction={'row'}
+          mt={1}
+          mb={3}
+          onClick={() => router.push('/help-center')}
+          gap="5px"
+          style={{ width: 'fit-content', cursor: 'pointer' }}>
+          <Stack direction={'column'} justifyContent={'center'}>
+            <BackIconNav fontSize="small" style={{ color: 'black', fontSize: '12px', fontWeight: 'bold' }} />
+          </Stack>
+          <Typography variant="h1" style={{ fontSize: 20, color: 'black' }}>
+            Kembali
+          </Typography>
+        </Stack>
+      </Stack>
+      <PageContainer heading="">
+        <Typography>Coming soon</Typography>
+      </PageContainer>
+    </>
+  );
+};
+
+export default PermohonanPremium;
