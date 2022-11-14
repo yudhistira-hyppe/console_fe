ab156ca403e3ad17b5ee62a1e66ba9840f3c7a17
ardihp <ardihp8@gmail.com>
2022-11-14 16:54:08 +0700

    fix(help-center): data key for progress indicator

--- modules/Pages/console/help-center/CardWithIndicator/ProgressIndicator.js
+++ modules/Pages/console/help-center/CardWithIndicator/ProgressIndicator.js
@@ -8,11 +8,11 @@ const ProgressIndicator = ({ item, ...rest }) => {
       <CmtProgressBar
         label={
           <Box display="flex" alignItems="center">
-            {`${item.label}`} | <Box pl={1} component="span" color="text.secondary" fontSize={12}>{`${item.rate}`}</Box>
+            {`${item.reason}`} | <Box pl={1} component="span" color="text.secondary" fontSize={12}>{`${item.count}`}</Box>
           </Box>
         }
         labelPos="top-left"
-        value={item.value}
+        value={item.persen === 'Infinity' ? 0 : item.persen}
         renderValue={(value) => {
           return `${value}%`;
         }}
