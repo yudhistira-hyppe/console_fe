c64779d14a716298b6758c041b642c0bc82a4ab4
ardihp <ardihp8@gmail.com>
2022-11-25 15:56:00 +0700

    fix(database): redirect to signin page when unauthorize

--- pages/database/[...slug].js
+++ pages/database/[...slug].js
@@ -3,6 +3,7 @@ import { useRouter } from 'next/router';
 import dynamic from 'next/dynamic';
 import PageLoader from '@jumbo/components/PageComponents/PageLoader';
 import SecureConsolePage from 'authentication/auth-page-wrappers/SecureConsolePage';
+import { useAuth } from 'authentication';
 
 const DatabaseComponent = dynamic(() => import('modules/Pages/console/database'), {
   loading: () => <PageLoader />,
@@ -11,6 +12,7 @@ const DatabaseComponent = dynamic(() => import('modules/Pages/console/database')
 const validDatabaseTab = ['account', 'content', 'media'];
 
 const DatabaseDynamicPage = () => {
+  const { authUser } = useAuth();
   const router = useRouter();
   const { slug } = router.query;
   const [databaseProps, setDatabaseProps] = useState({});
@@ -19,6 +21,10 @@ const DatabaseDynamicPage = () => {
     if (slug) {
       if (slug.length > 2 || !validDatabaseTab.includes(slug[0])) {
         router.replace('/database/account');
+        return;
+      }
+      if (!authUser?.user) {
+        router.push({ pathname: '/signin', query: { redirect: router.asPath } });
         return;
       }
       setDatabaseProps({ tab: slug[0], detailId: slug[1] });
