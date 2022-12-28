4adc365d83a16b7c2b787339db5adf64e5483cf2
ardihp <ardihp8@gmail.com>
2022-12-28 15:55:39 +0700

    fix(auth): user access page

--- pages/database/index.js
+++ pages/database/index.js
@@ -3,9 +3,18 @@ import { useRouter } from 'next/router';
 
 const DatabasePage = () => {
   const router = useRouter();
+  const access = localStorage.getItem('access') ? JSON.parse(localStorage.getItem('access')) : [];
 
   useEffect(() => {
-    router.replace('/database/account');
+    if (access.map((item) => item?.nameModule).includes('database_account')) {
+      router.replace('/database/account');
+    } else if (access.map((item) => item?.nameModule).includes('database_content')) {
+      router.replace('/database/content');
+    } else if (access.map((item) => item?.nameModule).includes('database_media')) {
+      router.replace('/database/media');
+    } else {
+      router.replace('/');
+    }
   }, []);
 
   return null;
