8a8d7f318329889475b85e8db9c32151dcc7cbb5
ardihp <ardihp8@gmail.com>
2023-01-26 14:05:13 +0700

    fix(database): redirect to signin page for unauthorized user

--- pages/database/index.js
+++ pages/database/index.js
@@ -1,20 +1,26 @@
 import { useEffect } from 'react';
 import { useRouter } from 'next/router';
+import { useAuth } from 'authentication';
 
 const DatabasePage = () => {
+  const { authUser } = useAuth();
   const router = useRouter();
 
   useEffect(() => {
-    const access = localStorage.getItem('access') ? JSON.parse(localStorage.getItem('access')) : [];
+    if (authUser) {
+      const access = localStorage.getItem('access') ? JSON.parse(localStorage.getItem('access')) : [];
 
-    if (access.map((item) => item?.nameModule).includes('database_account')) {
-      router.replace('/database/account');
-    } else if (access.map((item) => item?.nameModule).includes('database_content')) {
-      router.replace('/database/content');
-    } else if (access.map((item) => item?.nameModule).includes('database_music')) {
-      router.replace('/database/media');
+      if (access.map((item) => item?.nameModule).includes('database_account')) {
+        router.replace('/database/account');
+      } else if (access.map((item) => item?.nameModule).includes('database_content')) {
+        router.replace('/database/content');
+      } else if (access.map((item) => item?.nameModule).includes('database_music')) {
+        router.replace('/database/media');
+      } else {
+        router.replace('/');
+      }
     } else {
-      router.replace('/');
+      router.replace('/signin');
     }
   }, []);
 
