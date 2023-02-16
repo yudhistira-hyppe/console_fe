d929ceaeb5618a3d6f23f58f55d33865d37110c0
ardihp <ardihp8@gmail.com>
2023-02-16 15:55:10 +0700

    fix(dashboard): data user active

--- modules/Pages/console/dashboards/UserActive/index.js
+++ modules/Pages/console/dashboards/UserActive/index.js
@@ -6,20 +6,23 @@ import UserActiveCard from './userActiveCard';
 import UserActiveGraph from './userActiveGraph';
 
 const UserActive = () => {
-  const [payload, setPayload] = useState({ date: moment().subtract(6, 'day').format('YYYY-MM-DD') });
+  const [payload, setPayload] = useState({
+    startdate: moment().subtract(6, 'day').format('YYYY-MM-DD'),
+    enddate: moment().format('YYYY-MM-DD'),
+  });
   const { data: userActive } = useGetUserActiveQuery(payload);
 
   const handlePayload = (value) => {
-    setPayload({ ...payload, date: moment().subtract(value, 'day').format('YYYY-MM-DD') });
+    setPayload({ ...payload, startdate: moment().subtract(value, 'day').format('YYYY-MM-DD') });
   };
 
   const totalActive = () => {
-    return userActive?.data?.data?.map((item) => item.totaldata).reduce((a, b) => a + b) || 0;
+    return userActive?.data?.map((item) => item.count).reduce((a, b) => a + b) || 0;
   };
 
   return (
     <UserActiveCard title="Pengguna Aktif" secondaryTitle="Bulan ini" amount={totalActive()} handlePayload={handlePayload}>
-      <UserActiveGraph data={userActive?.data?.data} />
+      <UserActiveGraph data={userActive?.data} />
     </UserActiveCard>
   );
 };
