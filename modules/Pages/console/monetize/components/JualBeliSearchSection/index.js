37711fe1813fed39aec246e4f06cd1123ed086c7
ardihp <ardihp8@gmail.com>
2023-02-20 14:20:24 +0700

    feat(monetize): integrate index data table jual beli content

--- modules/Pages/console/monetize/components/JualBeliSearchSection/index.js
+++ modules/Pages/console/monetize/components/JualBeliSearchSection/index.js
@@ -1,136 +1,343 @@
-import React from 'react';
-import useStyles from '../../../help-center/bantuan-pengguna/index.style';
+import React, { useEffect, useState } from 'react';
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import Checkbox from '@mui/material/Checkbox';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
-import { Box, Typography, Chip, FormGroup, FormControlLabel } from '@material-ui/core';
-import { Accordion, AccordionDetails, AccordionSummary, Stack, TextField, Checkbox } from '@mui/material';
-import { DateRangePicker, LocalizationProvider } from '@mui/x-date-pickers-pro';
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import TextField from '@mui/material/TextField';
+import { Box, Typography, Chip, FormGroup, FormControlLabel, makeStyles } from '@material-ui/core';
+import { Divider, IconButton, InputAdornment, Popover, Radio, RadioGroup, Stack } from '@mui/material';
+import { DateRange as DateRangePicker } from 'react-date-range';
+import { DateRange, RemoveCircleOutline } from '@material-ui/icons';
+import moment from 'moment';
 
-const SearchSection = ({ handleChange }) => {
+const useStyles = makeStyles((theme) => ({
+  inBuildAppCard: {
+    backgroundColor: theme.palette.background.paper,
+    borderRadius: 4,
+    boxShadow: '0px 1px 3px rgba(0, 0, 0, 0.2), 0px 2px 1px rgba(0, 0, 0, 0.12), 0px 1px 1px rgba(0, 0, 0, 0.14)',
+    position: 'relative',
+    overflow: 'hidden',
+    height: '100%',
+  },
+}));
+
+const SearchSection = ({ filter, handleChange, kind, setKind }) => {
   const classes = useStyles();
-  const [week, setWeek] = React.useState(null);
-  const [value, setValue] = React.useState([null, null]);
-  function getWeeksAfter(date, amount) {
-    return date && amount ? date.add(amount, 'week') : undefined;
-  }
+  const [week, setWeek] = useState(null);
+  const [value, setValue] = useState([
+    {
+      startDate: new Date(),
+      endDate: new Date(),
+      key: 'selection',
+    },
+  ]);
+  const [isDate, setDate] = useState(false);
+  const [anchorEl, setAnchorEl] = useState(null);
+
+  useEffect(() => {
+    if (!filter.createdAt[0] && !filter.createdAt[1]) {
+      setWeek(null);
+      setValue([
+        {
+          startDate: new Date(),
+          endDate: new Date(),
+          key: 'selection',
+        },
+      ]);
+      setDate(false);
+    }
+  }, [filter.createdAt, kind]);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const open = Boolean(anchorEl);
+  const id = open ? 'simple-popover' : undefined;
 
   return (
-    <Box className={classes.inBuildAppCard} p={5}>
-      <Accordion elevation={0} defaultExpanded>
-        <AccordionSummary expandIcon={<ExpandMoreIcon />} style={{ padding: '0px', minHeight: '0px' }}>
-          <Typography style={{ fontSize: '13px' }}>Tanggal Masuk</Typography>
-        </AccordionSummary>
-        <AccordionDetails style={{ padding: '0px' }}>
-          <Stack direction={'column'} spacing={1} mb={3}>
-            <div>
+    <>
+      <Box className={classes.inBuildAppCard} p={5} pt={2} maxWidth={270}>
+        <Accordion elevation={0} disableGutters defaultExpanded>
+          <AccordionSummary expandIcon={<ExpandMoreIcon />} style={{ padding: '0px', minHeight: '0px' }}>
+            <Typography style={{ fontSize: '13px' }}>Tanggal Transaksi</Typography>
+          </AccordionSummary>
+          <AccordionDetails style={{ padding: '0px' }}>
+            <Stack direction={'column'} spacing={1} mb={3}>
               <Chip
                 clickable
                 onClick={() => {
-                  handleChange('transaction_date', 7);
-                  setWeek(1), setValue([null, null]);
+                  if (week === 1) {
+                    setWeek(null);
+                    handleChange('createdAt', ['', '']);
+                    handleChange('labelTanggal', '');
+                    setDate(false);
+                  } else {
+                    setDate(true);
+                    handleChange('createdAt', [
+                      moment().subtract(7, 'd').format('YYYY-MM-DD'),
+                      moment().format('YYYY-MM-DD'),
+                    ]);
+                    handleChange('labelTanggal', '7 hari Terakhir');
+                    setWeek(1);
+                    setValue([
+                      {
+                        startDate: new Date().setDate(new Date().getDate() - 7),
+                        endDate: new Date(),
+                        key: 'selection',
+                      },
+                    ]);
+                  }
                 }}
                 label="7 Hari"
                 size="small"
+                style={{ width: 'fit-content', height: 35, padding: '0 8px' }}
                 variant={week == 1 ? 'default' : 'outlined'}
               />
-            </div>
-            <div>
               <Chip
                 label="14 Hari"
                 clickable
                 onClick={() => {
-                  handleChange('transaction_date', 14);
-                  setWeek(2), setValue([null, null]);
+                  if (week === 2) {
+                    setWeek(null);
+                    handleChange('createdAt', ['', '']);
+                    handleChange('labelTanggal', '');
+                    setDate(false);
+                  } else {
+                    setDate(true);
+                    handleChange('createdAt', [
+                      moment().subtract(14, 'd').format('YYYY-MM-DD'),
+                      moment().format('YYYY-MM-DD'),
+                    ]);
+                    handleChange('labelTanggal', '14 Hari Terakhir');
+                    setWeek(2);
+                    setValue([
+                      {
+                        startDate: new Date().setDate(new Date().getDate() - 14),
+                        endDate: new Date(),
+                        key: 'selection',
+                      },
+                    ]);
+                  }
                 }}
                 size="small"
+                style={{ width: 'fit-content', height: 35, padding: '0 8px' }}
                 variant={week === 2 ? 'default' : 'outlined'}
               />
-            </div>
-            <div>
               <Chip
                 label="1 Bulan"
                 clickable
                 onClick={() => {
-                  handleChange('transaction_date', 30);
-                  setWeek(4), setValue([null, null]);
+                  if (week === 4) {
+                    setWeek(null);
+                    handleChange('createdAt', ['', '']);
+                    handleChange('labelTanggal', '');
+                    setDate(false);
+                  } else {
+                    setDate(true);
+                    handleChange('createdAt', [
+                      moment().subtract(30, 'd').format('YYYY-MM-DD'),
+                      moment().format('YYYY-MM-DD'),
+                    ]);
+                    handleChange('labelTanggal', '1 Bulan Terakhir');
+                    setWeek(4);
+                    setValue([
+                      {
+                        startDate: new Date().setDate(new Date().getDate() - 30),
+                        endDate: new Date(),
+                        key: 'selection',
+                      },
+                    ]);
+                  }
                 }}
                 size="small"
+                style={{ width: 'fit-content', height: 35, padding: '0 8px' }}
                 variant={week === 4 ? 'default' : 'outlined'}
               />
-            </div>
-            <div>
               <Chip
                 label="3 Bulan"
                 clickable
                 onClick={() => {
-                  handleChange('transaction_date', 90);
-                  setWeek(12), setValue([null, null]);
+                  if (week === 12) {
+                    setWeek(null);
+                    handleChange('createdAt', ['', '']);
+                    handleChange('labelTanggal', '');
+                    setDate(false);
+                  } else {
+                    setDate(true);
+                    handleChange('createdAt', [
+                      moment().subtract(90, 'd').format('YYYY-MM-DD'),
+                      moment().format('YYYY-MM-DD'),
+                    ]);
+                    handleChange('labelTanggal', '3 Bulan Terakhir');
+                    setWeek(12);
+                    setValue([
+                      {
+                        startDate: new Date().setDate(new Date().getDate() - 90),
+                        endDate: new Date(),
+                        key: 'selection',
+                      },
+                    ]);
+                  }
                 }}
                 size="small"
+                style={{ width: 'fit-content', height: 35, padding: '0 8px' }}
                 variant={week === 12 ? 'default' : 'outlined'}
               />
-            </div>
-          </Stack>
+            </Stack>
 
-          <LocalizationProvider dateAdapter={AdapterDayjs} localeText={{ start: 'Start Date', end: 'End Date' }}>
-            <DateRangePicker
-              value={value}
-              maxDate={getWeeksAfter(value[0], week)}
-              onChange={(newValue) => {
-                handleChange('transaction_range', [newValue[0]?.format('YYYY-MM-DD'), newValue[1]?.format('YYYY-MM-DD')]);
-                setValue(newValue);
-              }}
-              renderInput={(startProps, endProps) => (
-                <>
-                  <Stack direction={'row'} spacing={1}>
-                    <TextField {...startProps} />
-                    <TextField {...endProps} />
-                  </Stack>
-                </>
+            <Stack direction="row" alignItems="center" spacing={1}>
+              <TextField
+                value={
+                  isDate
+                    ? `${moment(value[0]?.startDate).format('DD/MM/YYYY')} - ${moment(value[0]?.endDate).format(
+                        'DD/MM/YYYY',
+                      )}`
+                    : ''
+                }
+                placeholder="Pilih Tanggal"
+                autoComplete="off"
+                color="secondary"
+                onClick={handleClick}
+                InputProps={{
+                  startAdornment: (
+                    <InputAdornment position="start">
+                      <DateRange />
+                    </InputAdornment>
+                  ),
+                }}
+              />
+              {isDate && (
+                <IconButton
+                  style={{ height: 30, width: 30 }}
+                  onClick={() => {
+                    setValue([
+                      {
+                        startDate: new Date(),
+                        endDate: new Date(),
+                        key: 'selection',
+                      },
+                    ]);
+                    handleChange('createdAt', ['', '']);
+                    handleChange('labelTanggal', '');
+                    setDate(false);
+                  }}>
+                  <RemoveCircleOutline color="primary" />
+                </IconButton>
               )}
-            />
-          </LocalizationProvider>
-        </AccordionDetails>
-      </Accordion>
+            </Stack>
 
-      <Accordion elevation={0} defaultExpanded>
-        <AccordionSummary expandIcon={<ExpandMoreIcon />} style={{ padding: '0px' }}>
-          <Typography style={{ fontSize: '13px' }}>Masa Berlaku</Typography>
-        </AccordionSummary>
-        <AccordionDetails style={{ padding: 0 }}>
-          <FormGroup>
-            <FormControlLabel label={'> 30 Hari'} control={<Checkbox defaultChecked={false} />} />
-            <FormControlLabel label={'30-60 Hari'} control={<Checkbox defaultChecked={false} />} />
-            <FormControlLabel label={'61-90 Hari'} control={<Checkbox defaultChecked={false} />} />
-            <FormControlLabel label={'90 Hari <'} control={<Checkbox defaultChecked={false} />} />
-          </FormGroup>
-        </AccordionDetails>
-      </Accordion>
+            <Popover
+              id={id}
+              open={open}
+              anchorEl={anchorEl}
+              onClose={handleClose}
+              anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'left',
+              }}>
+              <DateRangePicker
+                onChange={(item) => {
+                  setValue([item.selection]);
+                  handleChange('createdAt', [
+                    moment(item.selection.startDate).format('YYYY-MM-DD'),
+                    item.selection.endDate ? moment(item.selection.endDate).format('YYYY-MM-DD') : '',
+                  ]);
+                  handleChange(
+                    'labelTanggal',
+                    `${moment(item.selection.startDate).format('DD-MM-YYYY')} - ${
+                      item.selection.endDate ? moment(item.selection.endDate).format('DD-MM-YYYY') : ''
+                    }`,
+                  );
+                  setDate(true);
+                  setWeek(null);
+                }}
+                dragSelectionEnabled={false}
+                moveRangeOnFirstSelection={false}
+                editableDateInputs={true}
+                ranges={value}
+                direction="horizontal"
+              />
+            </Popover>
+          </AccordionDetails>
+          <Divider style={{ marginTop: 16 }} />
+        </Accordion>
 
-      <Accordion elevation={0} defaultExpanded>
-        <AccordionSummary expandIcon={<ExpandMoreIcon />} style={{ padding: '0px' }}>
-          <Typography style={{ fontSize: '13px' }}>Status Pembayaran</Typography>
-        </AccordionSummary>
-        <AccordionDetails style={{ padding: '0px' }}>
-          <FormGroup>
-            <FormControlLabel
-              label={'Menuggu'}
-              control={
-                <Checkbox defaultChecked={false} onChange={() => handleChange('status_payment', 'WAITING_PAYMENT')} />
-              }
-            />
-            <FormControlLabel
-              label={'Lunas'}
-              control={<Checkbox defaultChecked={false} onChange={() => handleChange('status_payment', 'Success')} />}
-            />
-            <FormControlLabel
-              label={'Gagal'}
-              control={<Checkbox defaultChecked={false} onChange={() => handleChange('status_payment', 'Cancel')} />}
-            />
-          </FormGroup>
-        </AccordionDetails>
-      </Accordion>
-    </Box>
+        <Accordion elevation={0} disableGutters defaultExpanded>
+          <AccordionSummary expandIcon={<ExpandMoreIcon />} style={{ padding: '0px' }}>
+            <Typography style={{ fontSize: '13px' }}>Tipe Transaksi</Typography>
+          </AccordionSummary>
+          <AccordionDetails style={{ padding: 0 }}>
+            <RadioGroup
+              aria-labelledby="demo-radio-buttons-group-label"
+              defaultValue={kind}
+              name="kind"
+              onChange={(e) => {
+                setKind(e.target.value);
+                handleChange('kind');
+                setValue([null, null]);
+                setWeek(null);
+              }}>
+              <FormControlLabel value="sell" label={'Penjualan'} control={<Radio color="secondary" />} />
+              <FormControlLabel value="buy" label={'Pembelian'} control={<Radio color="secondary" />} />
+            </RadioGroup>
+          </AccordionDetails>
+          <Divider style={{ marginTop: 16 }} />
+        </Accordion>
+
+        <Accordion elevation={0} disableGutters defaultExpanded>
+          <AccordionSummary expandIcon={<ExpandMoreIcon />} style={{ padding: '0px' }}>
+            <Typography style={{ fontSize: '13px' }}>Status Voucher</Typography>
+          </AccordionSummary>
+          <AccordionDetails style={{ padding: 0 }}>
+            <FormGroup onChange={(e) => handleChange('status', e.target.value)}>
+              {/* <FormControlLabel
+                label={<Typography style={{ fontSize: 14 }}>Berhasil</Typography>}
+                control={
+                  <Checkbox
+                    defaultChecked={false}
+                    checked={filter.status.includes('Berhasil')}
+                    value={'Berhasil'}
+                    color="secondary"
+                  />
+                }
+              />
+              <FormControlLabel
+                label={<Typography style={{ fontSize: 14 }}>{kind === 'pembelian' ? 'Gagal' : 'Ditolak'}</Typography>}
+                control={
+                  <Checkbox
+                    defaultChecked={false}
+                    checked={filter.status.includes(kind === 'pembelian' ? 'Gagal' : 'Ditolak')}
+                    value={kind === 'pembelian' ? 'Gagal' : 'Ditolak'}
+                    color="secondary"
+                  />
+                }
+              />
+              <FormControlLabel
+                label={
+                  <Typography style={{ fontSize: 14 }}>
+                    {kind === 'pembelian' ? 'Menunggu Pembayaran' : 'Menunggu Approval'}
+                  </Typography>
+                }
+                control={
+                  <Checkbox
+                    defaultChecked={false}
+                    checked={filter.status.includes(kind === 'pembelian' ? 'Menunggu Pembayaran' : 'Menunggu Approval')}
+                    value={kind === 'pembelian' ? 'Menunggu Pembayaran' : 'Menunggu Approval'}
+                    color="secondary"
+                  />
+                }
+              /> */}
+            </FormGroup>
+          </AccordionDetails>
+        </Accordion>
+      </Box>
+    </>
   );
 };
 
--- modules/Pages/console/monetize/components/JualBeliTableSection/index.js
+++ modules/Pages/console/monetize/components/JualBeliTableSection/index.js
@@ -11,23 +11,37 @@ import {
   Paper,
   Chip,
 } from '@material-ui/core';
-import { Stack } from '@mui/material';
+import { CircularProgress, Stack } from '@mui/material';
 import MenuItem from '@mui/material/MenuItem';
 import Select from '@mui/material/Select';
 import FormControl from '@mui/material/FormControl';
 import Pagination from '@mui/material/Pagination';
 import moment from 'moment';
+import numberWithCommas from 'modules/Components/CommonComponent/NumberWithCommas/NumberWithCommas';
 
-const TableSection = ({ order, handleOrder, handlePageChange, listKepemilikan }) => {
-  const dataList = [];
-
+const TableSection = ({
+  order,
+  handleOrder,
+  handlePageChange,
+  loading,
+  listTransaction,
+  filterList,
+  handleDeleteFilter,
+  kind,
+}) => {
   return (
     <Stack flex={1}>
       <Box display={'flex'} flexDirection={'row'} justifyContent={'space-between'} mb={5}>
         <Box flex={1} flexDirection={'column'} justifyContent={'center'} display={'flex'}>
-          <Typography>
-            Menampilkan {10} hasil ({1} - {10} dari {20})
-          </Typography>
+          {loading ? (
+            <Typography style={{ fontFamily: 'Normal' }}>loading data...</Typography>
+          ) : (
+            <Typography style={{ fontFamily: 'Normal' }}>
+              Menampilkan {listTransaction?.total} hasil (
+              {listTransaction?.totalsearch >= 1 ? listTransaction?.page * 10 + 1 : listTransaction?.page * 10} -{' '}
+              {listTransaction?.total + listTransaction?.page * 10} dari {listTransaction?.totalsearch})
+            </Typography>
+          )}
         </Box>
         <Stack direction={'row'} spacing={2} style={{ flex: 1 }} justifyContent={'flex-end'}>
           <Box display={'flex'} flexDirection={'column'} justifyContent={'center'}>
@@ -39,68 +53,135 @@ const TableSection = ({ order, handleOrder, handlePageChange, listKepemilikan })
               value={order}
               inputProps={{ 'aria-label': 'Without label' }}
               style={{ backgroundColor: '#FFFFFF' }}>
-              <MenuItem value="desc">Terbaru</MenuItem>
-              <MenuItem value="asc">Terlama</MenuItem>
+              <MenuItem value="true">Terbaru</MenuItem>
+              <MenuItem value="false">Terlama</MenuItem>
             </Select>
           </FormControl>
         </Stack>
       </Box>
 
+      {filterList?.length >= 1 && (
+        <Stack direction="row" gap="10px" mb={2}>
+          {filterList?.map((item, key) => (
+            <Chip
+              key={key}
+              label={item.value}
+              onDelete={() => {
+                if (item.parent === 'search') {
+                  handleDeleteFilter(item.parent, '');
+                } else if (item.parent === 'createdAt') {
+                  handleDeleteFilter(item.parent, [null, null]);
+                } else if (item.parent === 'period') {
+                  handleDeleteFilter('clearRange', []);
+                } else if (item.parent === 'payment_status') {
+                  handleDeleteFilter(item.parent, JSON.stringify({ name: item.value }));
+                } else {
+                  handleDeleteFilter(item.parent, item.value);
+                }
+              }}
+            />
+          ))}
+        </Stack>
+      )}
+
       <TableContainer component={Paper}>
+        <Typography
+          variant="h3"
+          style={{ fontFamily: 'Lato', padding: '20px 16px', boxShadow: '0px 0px 1px rgba(0, 0, 0, 0.161741)' }}>
+          {kind === 'sell' ? 'Penjualan' : 'Pembelian'}
+        </Typography>
         <Table sx={{ minWidth: 650 }} aria-label="basic-table">
           <TableHead>
             <TableRow>
               <TableCell>Waktu Transaksi</TableCell>
               <TableCell align="left">Invoice Transaksi</TableCell>
+              <TableCell align="left" style={{ maxWidth: 200 }}>
+                Konten
+              </TableCell>
               <TableCell align="left">Harga</TableCell>
-              <TableCell align="left">Pembeli</TableCell>
+              <TableCell align="left">{kind === 'sell' ? 'Penjual' : 'Pembeli'}</TableCell>
               <TableCell align="left">Status Pembayaran</TableCell>
             </TableRow>
           </TableHead>
 
           <TableBody>
-            <TableRow key={1} sx={{ '&:last-child td, &:last-child th': { border: 0 } }} style={{ cursor: 'pointer' }} hover>
-              <TableCell component="th" scope="row">
-                <Typography variant="body1" style={{ fontSize: '12px' }}>
-                  {moment().format('lll')}
-                </Typography>
+            {loading ? (
+              <TableCell colSpan={8}>
+                <Stack direction="column" alignItems="center" justifyContent="center" height={468} spacing={2}>
+                  <CircularProgress color="secondary" />
+                  <Typography style={{ fontFamily: 'Normal' }}>loading data...</Typography>
+                </Stack>
               </TableCell>
-              <TableCell align="left">
-                <Typography variant="body1" style={{ fontSize: '12px' }}>
-                  INV/MMXXII/IX/XXVI/20
-                </Typography>
-              </TableCell>
-              <TableCell align="left">
-                <Typography variant="body1" style={{ fontSize: '12px' }}>
-                  Rp 200000
-                </Typography>
-              </TableCell>
-              <TableCell align="left">
-                <Typography variant="body1" style={{ fontSize: '12px' }}>
-                  Joko
-                </Typography>
-              </TableCell>
-              <TableCell align="left">
-                <Chip label="Menunggu" style={{ backgroundColor: 'rgba(255, 140, 0, 0.15)', color: '#FF8C00D9' }} />
-                {/* {item?.status === 'Success' && (
-                        <Chip label="Lunas" style={{ backgroundColor: 'rgba(113, 165, 0, 0.1)', color: '#71A500D9' }} />
-                      )}
-                      {item?.status === 'Cancel' && (
-                        <Chip label="Gagal" style={{ backgroundColor: 'rgba(103, 103, 103, 0.1)', color: '#676767D9' }} />
-                      )} */}
-              </TableCell>
-            </TableRow>
-            {/* <TableRow>
+            ) : listTransaction?.data?.length >= 1 ? (
+              listTransaction?.data?.map((item, key) => (
+                <TableRow key={key} sx={{ '&:last-child td, &:last-child th': { border: 0 } }} hover>
+                  <TableCell component="th" scope="row">
+                    <Typography variant="body1" style={{ fontSize: '12px' }}>
+                      {moment(item?.timestamp).format('lll')}
+                    </Typography>
+                  </TableCell>
+                  <TableCell align="left">
+                    <Typography variant="body1" style={{ fontSize: '12px' }}>
+                      {item?.noinvoice}
+                    </Typography>
+                  </TableCell>
+                  <TableCell align="left" style={{ maxWidth: 200 }}>
+                    <Typography
+                      variant="body1"
+                      style={{
+                        fontSize: '12px',
+                        width: '100%',
+                        whiteSpace: 'nowrap',
+                        textOverflow: 'ellipsis',
+                        overflow: 'hidden',
+                      }}
+                      title={item?.descriptionContent}>
+                      {item?.descriptionContent}
+                    </Typography>
+                  </TableCell>
+                  <TableCell align="left">
+                    <Typography variant="body1" style={{ fontSize: '12px' }}>
+                      Rp {numberWithCommas(item?.totalamount || 0)}
+                    </Typography>
+                  </TableCell>
+                  <TableCell align="left">
+                    <Typography variant="body1" style={{ fontSize: '12px' }}>
+                      {'-'}
+                    </Typography>
+                  </TableCell>
+                  <TableCell align="left">
+                    {item?.status === 'WAITING_PAYMENT' && (
+                      <Chip label="Menunggu Pembayaran" style={{ backgroundColor: '#0356AF1A', color: '#0356AF' }} />
+                    )}
+                    {item?.status === 'Success' && (
+                      <Chip label="Berhasil" style={{ backgroundColor: 'rgba(113, 165, 0, 0.1)', color: '#71A500D9' }} />
+                    )}
+                    {item?.status === 'Cancel' && (
+                      <Chip label="Gagal" style={{ backgroundColor: 'rgba(103, 103, 103, 0.1)', color: '#676767D9' }} />
+                    )}
+                  </TableCell>
+                </TableRow>
+              ))
+            ) : (
+              <TableRow>
                 <TableCell colSpan={6} align="center">
                   Tidak ada data.
                 </TableCell>
-              </TableRow> */}
+              </TableRow>
+            )}
           </TableBody>
         </Table>
       </TableContainer>
-      <Stack alignItems={'center'} mt={2}>
-        <Pagination count={1} size={'small'} onChange={handlePageChange} />
-      </Stack>
+      {listTransaction?.totalsearch >= 1 && !loading && (
+        <Stack alignItems={'center'} mt={2}>
+          <Pagination
+            count={Number(listTransaction?.totalpage)}
+            page={listTransaction?.page + 1}
+            size={'small'}
+            onChange={handlePageChange}
+          />
+        </Stack>
+      )}
     </Stack>
   );
 };
