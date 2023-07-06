a3b91c27a8791da36769b59f44f0e4437fbf761a
ardihp <ardihp8@gmail.com>
2023-07-06 15:58:52 +0700

    feat(ads): custom modal choose date on setting ads tab dashboard

--- modules/Pages/console/ads-center/setting/tab/dashboard/index.js
+++ modules/Pages/console/ads-center/setting/tab/dashboard/index.js
@@ -6,10 +6,10 @@ import { useGetDashboardAdsSettingQuery } from 'api/console/ads';
 import dayjs from 'dayjs';
 import PageLoader from '@jumbo/components/PageComponents/PageLoader';
 import { Typography } from '@material-ui/core';
-import { DateRange as DateRangePicker } from 'react-date-range';
 import { DateRange, RemoveCircleOutline } from '@material-ui/icons';
 import 'react-date-range/dist/styles.css';
 import 'react-date-range/dist/theme/default.css';
+import ModalChooseDate from './ModalChooseDate';
 
 const AdsSettingDashboard = () => {
   const [payload, setPayload] = useState([
@@ -35,76 +35,59 @@ const AdsSettingDashboard = () => {
   };
 
   const open = Boolean(anchorEl);
-  const id = open ? 'simple-popover' : undefined;
 
   return (
     <PageContainer>
-      <Stack direction="row" justifyContent="space-between" alignItems="center" mb={4} mt="-76px">
-        <Box />
-        <>
-          <Stack direction="row" alignItems="center" spacing={1}>
-            <TextField
-              value={
-                isDate
-                  ? `${dayjs(payload[0]?.startDate).format('DD/MM/YYYY')} - ${dayjs(payload[0]?.endDate).format(
-                      'DD/MM/YYYY',
-                    )}`
-                  : ''
-              }
-              placeholder="Pilih Tanggal"
-              autoComplete="off"
-              color="secondary"
-              size="small"
-              onClick={handleClick}
-              sx={{ input: { height: 30 } }}
-              InputProps={{
-                startAdornment: (
-                  <InputAdornment position="start">
-                    <DateRange />
-                  </InputAdornment>
-                ),
-              }}
-            />
-            {isDate && (
-              <IconButton
-                style={{ height: 30, width: 30 }}
-                onClick={() => {
-                  setPayload([
-                    {
-                      startDate: new Date(),
-                      endDate: new Date(),
-                      key: 'selection',
-                    },
-                  ]);
-                  setDate(false);
-                }}>
-                <RemoveCircleOutline color="primary" />
-              </IconButton>
-            )}
-          </Stack>
+      <Stack direction="row" justifyContent="flex-end" alignItems="center" mb={4} mt="-76px">
+        <Stack direction="row" alignItems="center" spacing={1}>
+          <TextField
+            value={
+              isDate
+                ? `${dayjs(payload[0]?.startDate).format('DD/MM/YYYY')} - ${dayjs(payload[0]?.endDate).format('DD/MM/YYYY')}`
+                : ''
+            }
+            placeholder="Pilih Tanggal"
+            autoComplete="off"
+            color="secondary"
+            size="small"
+            onClick={handleClick}
+            sx={{ input: { height: 30 } }}
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">
+                  <DateRange />
+                </InputAdornment>
+              ),
+            }}
+          />
+          {isDate && (
+            <IconButton
+              style={{ height: 30, width: 30 }}
+              onClick={() => {
+                setPayload([
+                  {
+                    startDate: new Date(),
+                    endDate: new Date(),
+                    key: 'selection',
+                  },
+                ]);
+                setDate(false);
+              }}>
+              <RemoveCircleOutline color="primary" />
+            </IconButton>
+          )}
+        </Stack>
 
-          <Popover
-            id={id}
-            open={open}
-            anchorEl={anchorEl}
-            onClose={handleClose}
-            anchorOrigin={{
-              vertical: 'bottom',
-              horizontal: 'left',
-            }}>
-            <DateRangePicker
-              onChange={(item) => {
-                setPayload([item.selection]);
-                setDate(true);
-              }}
-              dragSelectionEnabled={false}
-              moveRangeOnFirstSelection={false}
-              editableDateInputs={true}
-              ranges={payload}
-              direction="horizontal"
-            />
-          </Popover>
-        </>
+        <ModalChooseDate
+          open={open}
+          dataPayload={payload}
+          onClose={handleClose}
+          onSubmit={(val) => {
+            setPayload(val);
+            setDate(true);
+            setAnchorEl(null);
+          }}
+        />
       </Stack>
       {loadingData ? (
         <PageLoader />
