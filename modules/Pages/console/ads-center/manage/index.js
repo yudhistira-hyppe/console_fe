47ee4364f1d9dfad4bd7297ce407f93ad3e99917
ardihp <ardihp8@gmail.com>
2023-07-12 11:23:51 +0700

    fix(ads): manage page tab value using query tab value

--- modules/Pages/console/ads-center/manage/index.js
+++ modules/Pages/console/ads-center/manage/index.js
@@ -1,21 +1,33 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Typography } from '@material-ui/core';
 import { Stack, Tab } from '@mui/material';
 import { TabContext, TabList, TabPanel } from '@mui/lab';
 import useStyles from './index.style';
 import AdsManageDashboard from './tab/dashboard';
 import AdsManageTableList from './tab/iklan';
+import Router from 'next/router';
 
 const AdsCenterManageComponent = () => {
   const classes = useStyles();
   const [tab, setTab] = useState('1');
+
+  useEffect(() => {
+    if (Router.query?.tab) {
+      setTab(Router.query?.tab);
+    } else {
+      setTab('1');
+    }
+  }, [Router]);
 
   return (
     <Stack direction="column">
       <Typography style={{ fontWeight: 'bold', fontSize: 20 }}>Kelola Advertiser</Typography>
       <TabContext value={tab}>
         <TabList
-          onChange={(_, selectedTab) => setTab(selectedTab)}
+          onChange={(_, selectedTab) => {
+            setTab(selectedTab);
+            Router.replace({ pathname: '/ads-center/manage', query: { tab: selectedTab } });
+          }}
           textColor="secondary"
           indicatorColor="secondary"
           style={{ margin: '5px 0' }}>
