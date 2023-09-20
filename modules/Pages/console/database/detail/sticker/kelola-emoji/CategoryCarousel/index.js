4efe8e3da1ea49617c037705767fd5dcd0ee0f75
ardihp <ardihp8@gmail.com>
2023-09-20 16:20:09 +0700

    feat(database): integrate list kelola emoji by category & modal update index

--- modules/Pages/console/database/detail/sticker/kelola-emoji/CategoryCarousel/index.js
+++ modules/Pages/console/database/detail/sticker/kelola-emoji/CategoryCarousel/index.js
@@ -2,7 +2,7 @@ import { IconButton, Stack } from '@mui/material';
 import React from 'react';
 import Carousel from 'react-multi-carousel';
 import 'react-multi-carousel/lib/styles.css';
-import { ChevronLeft, ChevronRight } from '@material-ui/icons';
+import { ArrowBack, ArrowForward } from '@material-ui/icons';
 import { Typography } from '@material-ui/core';
 
 const responsive = {
@@ -12,35 +12,60 @@ const responsive = {
   },
 };
 
+const CustomButtonGroup = ({ next, previous, ...rest }) => {
+  const {
+    carouselState: { currentSlide, slidesToShow, totalItems },
+  } = rest;
+
+  return (
+    <Stack direction="row" alignItems="center">
+      {currentSlide !== 0 && (
+        <IconButton
+          style={{
+            position: 'absolute',
+            height: 40,
+            width: 40,
+            top: 20,
+            left: -12,
+            background: 'white',
+            boxShadow: '0px 4px 5px rgba(0, 0, 0, 0.14), 0px 1px 10px rgba(0, 0, 0, 0.12), 0px 2px 4px rgba(0, 0, 0, 0.2)',
+          }}
+          onClick={() => previous()}>
+          <ArrowBack style={{ fontSize: 20 }} />
+        </IconButton>
+      )}
+      {currentSlide + slidesToShow !== totalItems && totalItems >= slidesToShow && (
+        <IconButton
+          style={{
+            position: 'absolute',
+            height: 40,
+            width: 40,
+            top: 20,
+            right: -12,
+            background: 'white',
+            boxShadow: '0px 4px 5px rgba(0, 0, 0, 0.14), 0px 1px 10px rgba(0, 0, 0, 0.12), 0px 2px 4px rgba(0, 0, 0, 0.2)',
+          }}
+          onClick={() => next()}>
+          <ArrowForward style={{ fontSize: 20 }} />
+        </IconButton>
+      )}
+    </Stack>
+  );
+};
+
 const CategoryCarousel = ({ data, tab, setTab }) => {
   return (
-    <Stack mt={3}>
+    <Stack mt={3} style={{ position: 'relative' }}>
       <Carousel
         responsive={responsive}
         swipeable={false}
         draggable={false}
-        customRightArrow={
-          <IconButton
-            style={{
-              position: 'absolute',
-              right: 4,
-              background: 'white',
-              boxShadow: '0px 4px 5px rgba(0, 0, 0, 0.14), 0px 1px 10px rgba(0, 0, 0, 0.12), 0px 2px 4px rgba(0, 0, 0, 0.2)',
-            }}>
-            <ChevronRight />
-          </IconButton>
-        }
-        customLeftArrow={
-          <IconButton
-            style={{
-              position: 'absolute',
-              left: 4,
-              background: 'white',
-              boxShadow: '0px 4px 5px rgba(0, 0, 0, 0.14), 0px 1px 10px rgba(0, 0, 0, 0.12), 0px 2px 4px rgba(0, 0, 0, 0.2)',
-            }}>
-            <ChevronLeft />
-          </IconButton>
-        }>
+        rtl={false}
+        arrows={false}
+        customButtonGroup={<CustomButtonGroup />}
+        renderArrowsWhenDisabled={false}
+        renderButtonGroupOutside
+        renderDotsOutside={false}>
         {data.map((item, key) => (
           <Stack
             key={key}
@@ -49,8 +74,9 @@ const CategoryCarousel = ({ data, tab, setTab }) => {
             alignItems="center"
             style={{
               background: 'linear-gradient(90deg, #610164 0%, #AB22AF 100%)',
-              width: 290,
+              width: '95%',
               height: 80,
+              margin: '0 auto',
               borderRadius: 8,
               padding: '12px 16px',
               flexShrink: 0,
@@ -80,7 +106,7 @@ const CategoryCarousel = ({ data, tab, setTab }) => {
               }}>
               {item.name}
             </Typography>
-            <img src={`/images/emoji/${item.image}`} style={{ width: 'auto', height: 45 }} />
+            <img src={item?.icon} style={{ width: 'auto', height: 45 }} />
           </Stack>
         ))}
       </Carousel>
