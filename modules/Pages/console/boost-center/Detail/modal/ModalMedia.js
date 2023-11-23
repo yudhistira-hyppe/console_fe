b2325c757105826cad1a20f6f5618ccb2f1ddc47
ardihp <ardihp8@gmail.com>
2023-11-23 11:03:39 +0700

    feat(boost): modal media when click thumbnail on detail page

---
+++ modules/Pages/console/boost-center/Detail/modal/ModalMedia.js
@@ -0,0 +1,43 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Modal from '@mui/material/Modal';
+import CardMedia from '@material-ui/core/CardMedia';
+import { useGetVideoFromApsaraQuery } from 'api/console/ads';
+
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  bgcolor: 'background.paper',
+  height: '60vh',
+  boxShadow: 24,
+  p: 0,
+  borderRadius: '4px',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+};
+
+export default function ModalMedia({ showModal, onClose, contentType, idApsara, urlImage }) {
+  const { data: apsaraVideo } = useGetVideoFromApsaraQuery({ apsaraId: idApsara });
+
+  return (
+    <div>
+      <Modal
+        open={showModal}
+        onClose={onClose}
+        disableAutoFocus
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description">
+        <Box sx={style}>
+          {contentType === 'video' ? (
+            <video src={apsaraVideo?.PlayUrl} controls height="100%" />
+          ) : (
+            <img src={urlImage} alt="image boost post" height="100%" />
+          )}
+        </Box>
+      </Modal>
+    </div>
+  );
+}
