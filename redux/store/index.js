bdcd838c0ef72cce0ca491b3c1b934cd8f283e39
ardihp <ardihp8@gmail.com>
2023-12-06 14:05:07 +0700

    fix(redux): disabled serializable check

--- redux/store/index.js
+++ redux/store/index.js
@@ -37,7 +37,7 @@ const initStore = () => {
   const store = configureStore({
     reducer: reducers,
     middleware: (getDefaultMiddleware) =>
-      getDefaultMiddleware().concat([
+      getDefaultMiddleware({ serializableCheck: false }).concat([
         authApi.middleware,
         dashboardApi.middleware,
         engagementApi.middleware,
