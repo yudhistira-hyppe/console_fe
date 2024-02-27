d9cbe9a8ba4a7f478ece3a45b8da288b2d63a5ad
ardihp <ardihp8@gmail.com>
2024-02-27 16:57:27 +0700

    feat(api): utilitas community crud

--- api/console/utilitas/community.js
+++ api/console/utilitas/community.js
@@ -14,7 +14,44 @@ export const communityUtilityApi = createApi({
       }),
       providesTags: ['list'],
     }),
+    createCommunity: build.mutation({
+      query: (body) => ({
+        url: '/guidelines',
+        method: 'POST',
+        body: body,
+      }),
+      invalidatesTags: ['list'],
+    }),
+    getDetailCommunity: build.query({
+      query: (id) => ({
+        url: `/guidelines/${id}`,
+        method: 'GET',
+      }),
+      providesTags: ['detail'],
+    }),
+    updateCommunity: build.mutation({
+      query: (body) => ({
+        url: '/guidelines/update',
+        method: 'PUT',
+        body: body,
+      }),
+      invalidatesTags: ['list', 'detail'],
+    }),
+    ApproveCommunity: build.mutation({
+      query: (body) => ({
+        url: '/guidelines/approve',
+        method: 'POST',
+        body: body,
+      }),
+      invalidatesTags: ['list', 'detail'],
+    }),
   }),
 });
 
-export const { useGetListCommunityQuery } = communityUtilityApi;
+export const {
+  useGetListCommunityQuery,
+  useCreateCommunityMutation,
+  useGetDetailCommunityQuery,
+  useUpdateCommunityMutation,
+  useApproveCommunityMutation,
+} = communityUtilityApi;
