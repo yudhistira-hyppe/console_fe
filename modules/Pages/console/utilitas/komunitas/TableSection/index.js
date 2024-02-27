85c6fa7250468d5723c2845429ae3d3407d8dfde
ardihp <ardihp8@gmail.com>
2024-02-27 11:42:11 +0700

    feat(util): integrate community table list with filter

---
+++ modules/Pages/console/utilitas/komunitas/TableSection/index.js
@@ -0,0 +1,171 @@
+import React from 'react';
+import {
+  Avatar,
+  Chip,
+  CircularProgress,
+  IconButton,
+  Paper,
+  Stack,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+} from '@mui/material';
+import dayjs from 'dayjs';
+import { useRouter } from 'next/router';
+import ScrollBar from 'react-perfect-scrollbar';
+import { Typography } from '@material-ui/core';
+import { Edit } from '@material-ui/icons';
+import { STREAM_URL } from 'authentication/auth-provider/config';
+import { useAuth } from 'authentication';
+
+const TableSection = ({ loading, listData }) => {
+  const access = localStorage.getItem('access') ? JSON.parse(localStorage.getItem('access')) : [];
+  const router = useRouter();
+  const { authUser } = useAuth();
+
+  const getAvatar = (mediaEndpoint) => {
+    const authToken = `?x-auth-token=${authUser.token}&x-auth-user=${authUser.user.email}`;
+    const endpoint = mediaEndpoint?.split('_');
+
+    return `${STREAM_URL}${endpoint?.[0]}${authToken}`;
+  };
+
+  return (
+    <TableContainer component={Paper} style={{ height: '100%' }}>
+      <ScrollBar>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell align="left">Judul</TableCell>
+              <TableCell align="left">Terakhir Diperbarui</TableCell>
+              <TableCell align="left">Diajukan Oleh</TableCell>
+              <TableCell align="left">Disetujui Oleh</TableCell>
+              <TableCell align="left">Waktu Disetujui</TableCell>
+              <TableCell align="left">Status</TableCell>
+              <TableCell align="left"></TableCell>
+            </TableRow>
+          </TableHead>
+
+          {loading ? (
+            <TableCell colSpan={8}>
+              <Stack direction="column" alignItems="center" justifyContent="center" height={468} spacing={2}>
+                <CircularProgress color="secondary" />
+                <Typography style={{ fontFamily: 'Normal' }}>loading data...</Typography>
+              </Stack>
+            </TableCell>
+          ) : (
+            <TableBody>
+              {listData?.data?.length >= 1 ? (
+                listData?.data?.map((item, i) => (
+                  <TableRow key={i} hover style={{ height: 73 }}>
+                    <TableCell>
+                      <Typography
+                        variant="body1"
+                        style={{
+                          fontSize: 14,
+                          width: 200,
+                          whiteSpace: 'nowrap',
+                          textOverflow: 'ellipsis',
+                          overflow: 'hidden',
+                        }}>
+                        {item?.name || '-'}
+                      </Typography>
+                    </TableCell>
+                    <TableCell>
+                      <Stack direction="column">
+                        <Typography variant="body1" style={{ fontSize: 12, width: 130 }}>
+                          {dayjs(item?.updatedAt).format('DD/MM/YYYY')} -
+                        </Typography>
+                        <Typography variant="body1" style={{ fontSize: 12, width: 130 }}>
+                          {dayjs(item?.updatedAt).format('HH:mm')} WIB
+                        </Typography>
+                      </Stack>
+                    </TableCell>
+                    <TableCell align="left">
+                      <Stack direction="row" alignItems="center" gap={2}>
+                        <Avatar src={getAvatar(item?.creatorAvatar?.mediaEndpoint)} alt="User yang mengajukan" />
+                        <Typography variant="body1" style={{ fontSize: 14, width: 120 }}>
+                          {item?.creatorFullname || '-'}
+                        </Typography>
+                      </Stack>
+                    </TableCell>
+                    <TableCell align="left">
+                      <Stack direction="row" alignItems="center" gap={2}>
+                        <Avatar src={getAvatar(item?.approverAvatar?.mediaEndpoint)} alt="User yang menyetujui" />
+                        <Typography variant="body1" style={{ fontSize: 14, width: 120 }}>
+                          {item?.approverFullname || '-'}
+                        </Typography>
+                      </Stack>
+                    </TableCell>
+                    <TableCell align="left">
+                      <Stack direction="column">
+                        <Typography variant="body1" style={{ fontSize: 12, width: 110 }}>
+                          {dayjs(item?.approvedAt).format('DD/MM/YYYY')} -
+                        </Typography>
+                        <Typography variant="body1" style={{ fontSize: 12, width: 110 }}>
+                          {dayjs(item?.approvedAt).format('HH:mm')} WIB
+                        </Typography>
+                      </Stack>
+                    </TableCell>
+                    <TableCell align="left">
+                      <Stack direction="row" width={80}>
+                        {item?.status === 'APPROVED' ? (
+                          <Chip
+                            label="Live"
+                            style={{
+                              fontSize: 14,
+                              fontWeight: 'bold',
+                              fontFamily: 'Lato',
+                              color: '#71A500D9',
+                              backgroundColor: '#71A5001A',
+                            }}
+                          />
+                        ) : (
+                          <Chip
+                            label="Diajukan"
+                            style={{
+                              fontSize: 14,
+                              fontWeight: 'bold',
+                              fontFamily: 'Lato',
+                              color: '#FF8C00D9',
+                              backgroundColor: '#FF8C0026',
+                            }}
+                          />
+                        )}
+                      </Stack>
+                    </TableCell>
+                    <TableCell>
+                      {access?.find((item) => item?.nameModule === 'utilitas_setting')?.acces?.updateAcces && (
+                        <Stack direction="row" justifyContent="flex-end" gap={1} width="100%">
+                          <IconButton
+                            onClick={() =>
+                              router.push({ pathname: '/utilitas', query: { tab: 'community', _id: item?._id } })
+                            }>
+                            <Edit />
+                          </IconButton>
+                        </Stack>
+                      )}
+                    </TableCell>
+                  </TableRow>
+                ))
+              ) : (
+                <TableRow>
+                  <TableCell colSpan={8}>
+                    <Stack direction="column" alignItems="center" justifyContent="center" height={468} spacing={2}>
+                      <Typography style={{ fontFamily: 'Normal' }}>Tidak ada data</Typography>
+                    </Stack>
+                  </TableCell>
+                </TableRow>
+              )}
+            </TableBody>
+          )}
+        </Table>
+      </ScrollBar>
+    </TableContainer>
+  );
+};
+
+export default TableSection;
--- modules/Pages/console/utilitas/komunitas/index.js
+++ modules/Pages/console/utilitas/komunitas/index.js
@@ -1,172 +1,49 @@
 import { Typography } from '@material-ui/core';
-import { Add, Edit } from '@material-ui/icons';
-import {
-  Avatar,
-  Button,
-  Chip,
-  CircularProgress,
-  IconButton,
-  Paper,
-  Stack,
-  Table,
-  TableBody,
-  TableCell,
-  TableContainer,
-  TableHead,
-  TableRow,
-} from '@mui/material';
-import dayjs from 'dayjs';
+import { Add } from '@material-ui/icons';
+import { Button, Stack } from '@mui/material';
+import React, { useState } from 'react';
+import TableSection from './TableSection';
 import { useRouter } from 'next/router';
-import React from 'react';
-import ScrollBar from 'react-perfect-scrollbar';
+import SearchSection from './SearchSection';
+import { useGetListCommunityQuery } from 'api/console/utilitas/community';
 
 const TableList = () => {
+  const [filter, setFilter] = useState({
+    page: 0,
+    limit: 10,
+    descending: true,
+    name: '',
+    isActive: true,
+  });
   const access = localStorage.getItem('access') ? JSON.parse(localStorage.getItem('access')) : [];
   const router = useRouter();
 
+  const { data: listData, isFetching: loadingList } = useGetListCommunityQuery(filter);
+
+  const handleChange = (name, val) => {
+    setFilter((prevVal) => ({ ...prevVal, [name]: val }));
+  };
+
   return (
-    <Stack flex={1} direction="row" alignItems="flex-start" spacing={3} height="100%">
-      <Stack direction="column" width="100%" maxWidth={270}>
+    <Stack flex={1} direction="row" spacing={3}>
+      <Stack direction="column" height="100%" gap={3}>
+        <SearchSection filter={filter} handleChange={handleChange} />
+
         <Button
           variant="contained"
-          startIcon={<Add />}
+          endIcon={<Add />}
           color="secondary"
           onClick={() => router.push({ pathname: '/utilitas', query: { tab: 'community', create: true } })}
           sx={{ height: 40 }}
           disabled={!access?.find((item) => item?.nameModule === 'utilitas_setting')?.acces?.createAcces}>
-          <Typography style={{ fontFamily: 'Lato', fontSize: 14, fontWeight: 'bold', textTransform: 'capitalize' }}>
+          <Typography
+            style={{ fontFamily: 'Lato', fontSize: 14, fontWeight: 'bold', textTransform: 'capitalize', marginBottom: 3 }}>
             Tambah Data
           </Typography>
         </Button>
       </Stack>
 
-      {1 + 1 === 3 ? (
-        <Stack direction="column" alignItems="center" justifyContent="center" height="100%" spacing={2}>
-          <CircularProgress color="secondary" />
-          <Typography style={{ fontFamily: 'Normal' }}>loading data...</Typography>
-        </Stack>
-      ) : (
-        <TableContainer component={Paper}>
-          <ScrollBar>
-            <Table>
-              <TableHead>
-                <TableRow>
-                  <TableCell align="left">Judul</TableCell>
-                  <TableCell align="left">Terakhir Diperbarui</TableCell>
-                  <TableCell align="left">Diajukan Oleh</TableCell>
-                  <TableCell align="left">Disetujui Oleh</TableCell>
-                  <TableCell align="left">Waktu Disetujui</TableCell>
-                  <TableCell align="left">Status</TableCell>
-                  <TableCell align="left"></TableCell>
-                </TableRow>
-              </TableHead>
-
-              <TableBody>
-                {[{}]?.length >= 1 ? (
-                  [{}]?.map((item, i) => (
-                    <TableRow key={i} hover style={{ height: 73 }}>
-                      <TableCell>
-                        <Typography
-                          variant="body1"
-                          style={{
-                            fontSize: 14,
-                            width: 200,
-                            whiteSpace: 'nowrap',
-                            textOverflow: 'ellipsis',
-                            overflow: 'hidden',
-                          }}>
-                          Panduan Komunitas
-                        </Typography>
-                      </TableCell>
-                      <TableCell>
-                        <Stack direction="column">
-                          <Typography variant="body1" style={{ fontSize: 12, width: 130 }}>
-                            {dayjs().format('DD/MM/YYYY')} -
-                          </Typography>
-                          <Typography variant="body1" style={{ fontSize: 12, width: 130 }}>
-                            {dayjs().format('HH:mm')} WIB
-                          </Typography>
-                        </Stack>
-                      </TableCell>
-                      <TableCell align="left">
-                        <Stack direction="row" alignItems="center" gap={2}>
-                          <Avatar src={''} alt="User yang mengajukan" />
-                          <Typography variant="body1" style={{ fontSize: 14, width: 80 }}>
-                            Paramita S
-                          </Typography>
-                        </Stack>
-                      </TableCell>
-                      <TableCell align="left">
-                        <Stack direction="row" alignItems="center" gap={2}>
-                          <Avatar src={''} alt="User yang menyetujui" />
-                          <Typography variant="body1" style={{ fontSize: 14, width: 80 }}>
-                            Pandji
-                          </Typography>
-                        </Stack>
-                      </TableCell>
-                      <TableCell align="left">
-                        <Stack direction="column">
-                          <Typography variant="body1" style={{ fontSize: 12, width: 110 }}>
-                            {dayjs().format('DD/MM/YYYY')} -
-                          </Typography>
-                          <Typography variant="body1" style={{ fontSize: 12, width: 110 }}>
-                            {dayjs().format('HH:mm')} WIB
-                          </Typography>
-                        </Stack>
-                      </TableCell>
-                      <TableCell align="left">
-                        <Stack direction="row" width={80}>
-                          <Chip
-                            label="Live"
-                            style={{
-                              fontSize: 14,
-                              fontWeight: 'bold',
-                              fontFamily: 'Lato',
-                              color: '#71A500D9',
-                              backgroundColor: '#71A5001A',
-                            }}
-                          />
-
-                          {/* <Chip
-                            label="Draft"
-                            style={{
-                              fontSize: 14,
-                              fontWeight: 'bold',
-                              fontFamily: 'Lato',
-                              color: '#FF8C00D9',
-                              backgroundColor: '#FF8C0026',
-                            }}
-                          /> */}
-                        </Stack>
-                      </TableCell>
-                      <TableCell>
-                        {access?.find((item) => item?.nameModule === 'utilitas_setting')?.acces?.updateAcces && (
-                          <Stack direction="row" justifyContent="flex-end" gap={1} width="100%">
-                            <IconButton
-                              onClick={() =>
-                                router.push({ pathname: '/utilitas', query: { tab: 'community', _id: item?._id } })
-                              }>
-                              <Edit />
-                            </IconButton>
-                          </Stack>
-                        )}
-                      </TableCell>
-                    </TableRow>
-                  ))
-                ) : (
-                  <TableRow>
-                    <TableCell colSpan={8}>
-                      <Stack direction="column" alignItems="center" justifyContent="center" height={468} spacing={2}>
-                        <Typography style={{ fontFamily: 'Normal' }}>Tidak ada Riwayat Settings</Typography>
-                      </Stack>
-                    </TableCell>
-                  </TableRow>
-                )}
-              </TableBody>
-            </Table>
-          </ScrollBar>
-        </TableContainer>
-      )}
+      <TableSection loading={loadingList} listData={listData} />
     </Stack>
   );
 };
