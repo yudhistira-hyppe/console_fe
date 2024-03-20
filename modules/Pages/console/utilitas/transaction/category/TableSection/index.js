f2ffd0e95c6f8c488e9a54b780da25f0bb3e6c18
ardihp <ardihp8@gmail.com>
2024-03-21 06:32:36 +0700

    fix(util): integrate table list category

--- modules/Pages/console/utilitas/transaction/category/TableSection/index.js
+++ modules/Pages/console/utilitas/transaction/category/TableSection/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   TableContainer,
   Table,
@@ -13,117 +13,113 @@ import {
   CircularProgress,
   Stack,
 } from '@mui/material';
-import { Delete, Edit } from '@material-ui/icons';
+import { Delete, Edit, NavigateBefore, NavigateNext } from '@material-ui/icons';
 import Router from 'next/router';
 import ScrollBar from 'react-perfect-scrollbar';
 import { Typography } from '@material-ui/core';
+import moment from 'moment';
 
-const TableSection = ({ listTickets }) => {
+const TableSection = ({ listCategory, loading, handlePageChange, filter }) => {
+  const [showModal, setShowModal] = useState({
+    open: false,
+    selected: '',
+  });
+
   return (
-    <TableContainer component={Paper}>
-      <ScrollBar>
-        <Table>
-          <TableHead>
-            <TableRow>
-              <TableCell style={{ width: 160 }}>Thumbnails</TableCell>
-              <TableCell align="left">Nama Bank</TableCell>
-              <TableCell align="left">Kode Bank</TableCell>
-              <TableCell align="left">Jumlah Digit</TableCell>
-              <TableCell align="left">Status</TableCell>
-              <TableCell align="left"></TableCell>
-            </TableRow>
-          </TableHead>
+    <Stack direction="column">
+      <TableContainer component={Paper}>
+        <ScrollBar>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell>Tanggal Dibuat</TableCell>
+                <TableCell align="left">Code</TableCell>
+                <TableCell align="left">COA</TableCell>
+                <TableCell align="left">Tipe</TableCell>
+                <TableCell align="left">User</TableCell>
+                <TableCell align="left"></TableCell>
+              </TableRow>
+            </TableHead>
 
-          <TableBody>
-            {1 - 1 === 2 ? (
-              <TableCell colSpan={8}>
-                <Stack direction="column" alignItems="center" justifyContent="center" height={468} spacing={2}>
-                  <CircularProgress color="secondary" />
-                  <Typography style={{ fontFamily: 'Normal' }}>loading data...</Typography>
-                </Stack>
-              </TableCell>
-            ) : listTickets?.data?.length >= 1 ? (
-              listTickets?.data?.map((item, i) => (
-                <TableRow key={i} hover>
-                  <TableCell>
-                    <Stack direction="row" gap="15px" width={80}>
-                      <Avatar
-                        src={item?.bankIcon + '?m=' + new Date().getTime() || new Error()}
-                        variant="rounded"
-                        alt="X"
-                        style={{ width: 40, height: 'auto', marginLeft: 12 }}
-                      />
-                    </Stack>
-                  </TableCell>
-                  <TableCell align="left">
-                    <Typography variant="body1" style={{ fontSize: '12px', width: 120 }}>
-                      {item?.bankname || '-'}
-                    </Typography>
-                  </TableCell>
-                  <TableCell align="left">
-                    <Typography variant="body1" style={{ fontSize: '12px', width: 120 }}>
-                      {item?.bankcode || '-'}
-                    </Typography>
-                  </TableCell>
-                  <TableCell align="left">
-                    <Typography variant="body1" style={{ fontSize: '12px', width: 120 }}>
-                      {item?.jmlDigit || 0}
-                    </Typography>
-                  </TableCell>
-                  <TableCell align="left">
-                    <Stack direction="row" width={120}>
-                      {item?.isActive ? (
-                        <Chip
-                          label="Aktif"
-                          style={{
-                            fontSize: 14,
-                            fontWeight: 'bold',
-                            fontFamily: 'Lato',
-                            color: '#71A500D9',
-                            backgroundColor: '#71A5001A',
-                          }}
-                        />
-                      ) : (
-                        <Chip
-                          label="Tidak Aktif"
-                          style={{
-                            fontSize: 14,
-                            fontWeight: 'bold',
-                            fontFamily: 'Lato',
-                            color: '#676767D9',
-                            backgroundColor: '#6767671A',
-                          }}
-                        />
-                      )}
-                    </Stack>
-                  </TableCell>
-                  <TableCell align="left">
-                    <Stack direction="row" justifyContent="flex-end">
-                      <Stack direction="row">
-                        <IconButton onClick={() => Router.replace(`/utilitas?tab=bank&bankcode=${item?.bankcode}`)}>
-                          <Edit />
-                        </IconButton>
+            <TableBody>
+              {loading ? (
+                <TableCell colSpan={8}>
+                  <Stack direction="column" alignItems="center" justifyContent="center" height={468} spacing={2}>
+                    <CircularProgress color="secondary" />
+                    <Typography style={{ fontFamily: 'Normal' }}>loading data...</Typography>
+                  </Stack>
+                </TableCell>
+              ) : listCategory?.data?.length >= 1 ? (
+                listCategory?.data?.map((item, i) => (
+                  <TableRow key={i} hover>
+                    <TableCell align="left">
+                      <Typography variant="body1" style={{ fontSize: '12px', width: 150 }}>
+                        {moment(item?.createdAt).format('DD/MM/YYYY - HH:mm')} WIB
+                      </Typography>
+                    </TableCell>
+                    <TableCell align="left">
+                      <Typography variant="body1" style={{ fontSize: '12px', width: 120 }}>
+                        {item?.code || '-'}
+                      </Typography>
+                    </TableCell>
+                    <TableCell align="left">
+                      <Typography variant="body1" style={{ fontSize: '12px', width: 120 }}>
+                        {item?.coa || '-'}
+                      </Typography>
+                    </TableCell>
+                    <TableCell align="left">
+                      <Typography variant="body1" style={{ fontSize: '12px', width: 120 }}>
+                        {item?.type?.length >= 1 ? item?.type?.join(', ') : '-'}
+                      </Typography>
+                    </TableCell>
+                    <TableCell align="left">
+                      <Typography variant="body1" style={{ fontSize: '12px', width: 120 }}>
+                        {item?.user || '-'}
+                      </Typography>
+                    </TableCell>
+                    <TableCell align="left">
+                      <Stack direction="row" justifyContent="flex-end">
+                        <Stack direction="row">
+                          <IconButton onClick={() => Router.replace(`/utilitas?tab=bank&bankcode=${item?.bankcode}`)}>
+                            <Edit />
+                          </IconButton>
+                        </Stack>
+                        <Stack direction="row">
+                          <IconButton
+                            onClick={() => setShowModal((prevVal) => ({ ...prevVal, open: true, selected: item?._id }))}>
+                            <Delete />
+                          </IconButton>
+                        </Stack>
                       </Stack>
-                      <Stack direction="row">
-                        <IconButton onClick={() => setSelected(item)}>
-                          <Delete />
-                        </IconButton>
-                      </Stack>
-                    </Stack>
-                  </TableCell>
-                </TableRow>
-              ))
-            ) : (
-              <TableCell colSpan={8}>
-                <Stack direction="column" alignItems="center" justifyContent="center" height={368} spacing={2}>
-                  <Typography style={{ fontFamily: 'Normal' }}>Tidak ada data.</Typography>
-                </Stack>
-              </TableCell>
-            )}
-          </TableBody>
-        </Table>
-      </ScrollBar>
-    </TableContainer>
+                    </TableCell>
+                  </TableRow>
+                ))
+              ) : (
+                <TableCell colSpan={8}>
+                  <Stack direction="column" alignItems="center" justifyContent="center" height={368} spacing={2}>
+                    <Typography style={{ fontFamily: 'Normal' }}>Tidak ada data.</Typography>
+                  </Stack>
+                </TableCell>
+              )}
+            </TableBody>
+          </Table>
+        </ScrollBar>
+      </TableContainer>
+
+      {listCategory?.data?.length >= 1 && !loading && (
+        <Stack direction="row" alignItems="center" justifyContent="right" spacing={2} mt={2}>
+          <IconButton color="secondary" onClick={() => handlePageChange(filter.page - 1)} disabled={filter.page < 1}>
+            <NavigateBefore />
+          </IconButton>
+          <IconButton
+            color="secondary"
+            onClick={() => handlePageChange(filter.page + 1)}
+            disabled={listCategory?.data?.length < 5}>
+            <NavigateNext />
+          </IconButton>
+        </Stack>
+      )}
+    </Stack>
   );
 };
 
--- modules/Pages/console/utilitas/transaction/category/index.js
+++ modules/Pages/console/utilitas/transaction/category/index.js
@@ -1,10 +1,37 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import { Button, Stack } from '@mui/material';
 import { Typography } from '@material-ui/core';
 import { Add } from '@material-ui/icons';
 import TableSection from './TableSection';
+import { useGetListCategoryQuery } from 'api/console/utilitas/transaction';
 
 const TransactionCategory = () => {
+  const [filter, setFilter] = useState({
+    page: 0,
+    limit: 5,
+    descending: 'true',
+  });
+
+  const getParams = useCallback(() => {
+    let params = [];
+    params.push(`?pageNumber=${filter.page}`);
+    params.push(`pageRow=${filter.limit}`);
+    params.push(`order=${filter.descending === 'true' ? 'DESC' : 'ASC'}`);
+
+    return params?.join('&');
+  }, [filter]);
+
+  const { data: listCategory, isFetching: loadingCategory } = useGetListCategoryQuery(getParams());
+
+  const handlePageChange = (value) => {
+    setFilter((prevVal) => {
+      return {
+        ...prevVal,
+        page: value,
+      };
+    });
+  };
+
   return (
     <Stack direction="column" gap={2}>
       <Stack direction="row" alignItems="center" justifyContent="space-between">
@@ -15,7 +42,12 @@ const TransactionCategory = () => {
         </Button>
       </Stack>
 
-      <TableSection />
+      <TableSection
+        listCategory={listCategory}
+        loading={loadingCategory}
+        filter={filter}
+        handlePageChange={handlePageChange}
+      />
     </Stack>
   );
 };
