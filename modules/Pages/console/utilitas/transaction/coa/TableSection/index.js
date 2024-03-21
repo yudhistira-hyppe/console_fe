b9084d536c6c4623a71877e4c2cc900d702e9edf
ardihp <ardihp8@gmail.com>
2024-03-21 07:55:08 +0700

    feat(util): integrate transaction coa

--- modules/Pages/console/utilitas/transaction/coa/TableSection/index.js
+++ modules/Pages/console/utilitas/transaction/coa/TableSection/index.js
@@ -17,86 +17,49 @@ import { Delete, Edit } from '@material-ui/icons';
 import Router from 'next/router';
 import ScrollBar from 'react-perfect-scrollbar';
 import { Typography } from '@material-ui/core';
+import moment from 'moment';
 
-const TableSection = ({ listTickets }) => {
+const TableSection = ({ listCOA, loading, filter, handlePageChange }) => {
   return (
-    <TableContainer component={Paper}>
+    <TableContainer component={Paper} style={{ minHeight: 422 }}>
       <ScrollBar>
         <Table>
           <TableHead>
             <TableRow>
-              <TableCell style={{ width: 160 }}>Thumbnails</TableCell>
-              <TableCell align="left">Nama Bank</TableCell>
-              <TableCell align="left">Kode Bank</TableCell>
-              <TableCell align="left">Jumlah Digit</TableCell>
-              <TableCell align="left">Status</TableCell>
+              <TableCell>Tanggal Dibuat</TableCell>
+              <TableCell align="left">COA</TableCell>
+              <TableCell align="left">Sub COA</TableCell>
               <TableCell align="left"></TableCell>
             </TableRow>
           </TableHead>
 
           <TableBody>
-            {1 - 1 === 2 ? (
+            {loading ? (
               <TableCell colSpan={8}>
                 <Stack direction="column" alignItems="center" justifyContent="center" height={468} spacing={2}>
                   <CircularProgress color="secondary" />
                   <Typography style={{ fontFamily: 'Normal' }}>loading data...</Typography>
                 </Stack>
               </TableCell>
-            ) : listTickets?.data?.length >= 1 ? (
-              listTickets?.data?.map((item, i) => (
+            ) : listCOA?.data?.length >= 1 ? (
+              listCOA?.data?.map((item, i) => (
                 <TableRow key={i} hover>
-                  <TableCell>
-                    <Stack direction="row" gap="15px" width={80}>
-                      <Avatar
-                        src={item?.bankIcon + '?m=' + new Date().getTime() || new Error()}
-                        variant="rounded"
-                        alt="X"
-                        style={{ width: 40, height: 'auto', marginLeft: 12 }}
-                      />
-                    </Stack>
-                  </TableCell>
                   <TableCell align="left">
-                    <Typography variant="body1" style={{ fontSize: '12px', width: 120 }}>
-                      {item?.bankname || '-'}
+                    <Typography variant="body1" style={{ fontSize: '12px', width: 150 }}>
+                      {moment(item?.createdAt).format('DD/MM/YYYY - HH:mm')} WIB
                     </Typography>
                   </TableCell>
                   <TableCell align="left">
                     <Typography variant="body1" style={{ fontSize: '12px', width: 120 }}>
-                      {item?.bankcode || '-'}
+                      {item?.coa || '-'}
                     </Typography>
                   </TableCell>
                   <TableCell align="left">
-                    <Typography variant="body1" style={{ fontSize: '12px', width: 120 }}>
-                      {item?.jmlDigit || 0}
+                    <Typography variant="body1" style={{ fontSize: '12px', width: 320 }}>
+                      {item?.subCoa?.length >= 1 ? item?.subCoa?.map((item) => item?.name)?.join(', ') : '-'}
                     </Typography>
                   </TableCell>
-                  <TableCell align="left">
-                    <Stack direction="row" width={120}>
-                      {item?.isActive ? (
-                        <Chip
-                          label="Aktif"
-                          style={{
-                            fontSize: 14,
-                            fontWeight: 'bold',
-                            fontFamily: 'Lato',
-                            color: '#71A500D9',
-                            backgroundColor: '#71A5001A',
-                          }}
-                        />
-                      ) : (
-                        <Chip
-                          label="Tidak Aktif"
-                          style={{
-                            fontSize: 14,
-                            fontWeight: 'bold',
-                            fontFamily: 'Lato',
-                            color: '#676767D9',
-                            backgroundColor: '#6767671A',
-                          }}
-                        />
-                      )}
-                    </Stack>
-                  </TableCell>
+
                   <TableCell align="left">
                     <Stack direction="row" justifyContent="flex-end">
                       <Stack direction="row">
