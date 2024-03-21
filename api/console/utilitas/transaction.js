33ad23e63a55faf0ebf4298d8d27e32fae51f52c
ardihp <ardihp8@gmail.com>
2024-03-21 07:54:37 +0700

    feat(api): delete utility category transaction

--- api/console/utilitas/transaction.js
+++ api/console/utilitas/transaction.js
@@ -4,15 +4,31 @@ import { customBaseQueryWithHandleReauth } from 'api';
 export const transactionUtilityApi = createApi({
   reducerPath: 'utilitas/transaction',
   baseQuery: customBaseQueryWithHandleReauth,
-  tagTypes: ['list', 'detail'],
+  tagTypes: ['listCategory', 'detailCategory', 'listCOA', 'detailCOA'],
   endpoints: (build) => ({
     getListCategory: build.query({
       query: (params) => ({
         url: `/transactions/v2/categorys${params || ''}`,
         method: 'GET',
       }),
+      providesTags: ['listCategory'],
+    }),
+    getListCOA: build.query({
+      query: (params) => ({
+        url: `/transactions/v2/coa${params || ''}`,
+        method: 'GET',
+      }),
+      providesTags: ['listCOA'],
+    }),
+    deleteCategory: build.mutation({
+      query: (data) => ({
+        url: '/transactions/v2/categorys/delete',
+        method: 'POST',
+        body: data,
+      }),
+      invalidatesTags: ['listCategory'],
     }),
   }),
 });
 
-export const { useGetListCategoryQuery } = transactionUtilityApi;
+export const { useGetListCategoryQuery, useGetListCOAQuery, useDeleteCategoryMutation } = transactionUtilityApi;
