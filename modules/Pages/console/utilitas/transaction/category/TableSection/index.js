71e564b724b65312e7628cc9771672ab6d68f6a5
ardihp <ardihp8@gmail.com>
2024-03-21 07:55:28 +0700

    feat(util): delete utility category transaction

--- modules/Pages/console/utilitas/transaction/category/TableSection/index.js
+++ modules/Pages/console/utilitas/transaction/category/TableSection/index.js
@@ -18,6 +18,7 @@ import Router from 'next/router';
 import ScrollBar from 'react-perfect-scrollbar';
 import { Typography } from '@material-ui/core';
 import moment from 'moment';
+import ModalDeleteCategoryTransaction from '../../../Modal/ModalDeleteCategoryTransaction';
 
 const TableSection = ({ listCategory, loading, handlePageChange, filter }) => {
   const [showModal, setShowModal] = useState({
@@ -26,100 +27,110 @@ const TableSection = ({ listCategory, loading, handlePageChange, filter }) => {
   });
 
   return (
-    <Stack direction="column">
-      <TableContainer component={Paper}>
-        <ScrollBar>
-          <Table>
-            <TableHead>
-              <TableRow>
-                <TableCell>Tanggal Dibuat</TableCell>
-                <TableCell align="left">Code</TableCell>
-                <TableCell align="left">COA</TableCell>
-                <TableCell align="left">Tipe</TableCell>
-                <TableCell align="left">User</TableCell>
-                <TableCell align="left"></TableCell>
-              </TableRow>
-            </TableHead>
-
-            <TableBody>
-              {loading ? (
-                <TableCell colSpan={8}>
-                  <Stack direction="column" alignItems="center" justifyContent="center" height={468} spacing={2}>
-                    <CircularProgress color="secondary" />
-                    <Typography style={{ fontFamily: 'Normal' }}>loading data...</Typography>
-                  </Stack>
-                </TableCell>
-              ) : listCategory?.data?.length >= 1 ? (
-                listCategory?.data?.map((item, i) => (
-                  <TableRow key={i} hover>
-                    <TableCell align="left">
-                      <Typography variant="body1" style={{ fontSize: '12px', width: 150 }}>
-                        {moment(item?.createdAt).format('DD/MM/YYYY - HH:mm')} WIB
-                      </Typography>
-                    </TableCell>
-                    <TableCell align="left">
-                      <Typography variant="body1" style={{ fontSize: '12px', width: 120 }}>
-                        {item?.code || '-'}
-                      </Typography>
-                    </TableCell>
-                    <TableCell align="left">
-                      <Typography variant="body1" style={{ fontSize: '12px', width: 120 }}>
-                        {item?.coa || '-'}
-                      </Typography>
-                    </TableCell>
-                    <TableCell align="left">
-                      <Typography variant="body1" style={{ fontSize: '12px', width: 120 }}>
-                        {item?.type?.length >= 1 ? item?.type?.join(', ') : '-'}
-                      </Typography>
-                    </TableCell>
-                    <TableCell align="left">
-                      <Typography variant="body1" style={{ fontSize: '12px', width: 120 }}>
-                        {item?.user || '-'}
-                      </Typography>
-                    </TableCell>
-                    <TableCell align="left">
-                      <Stack direction="row" justifyContent="flex-end">
-                        <Stack direction="row">
-                          <IconButton onClick={() => Router.replace(`/utilitas?tab=bank&bankcode=${item?.bankcode}`)}>
-                            <Edit />
-                          </IconButton>
-                        </Stack>
-                        <Stack direction="row">
-                          <IconButton
-                            onClick={() => setShowModal((prevVal) => ({ ...prevVal, open: true, selected: item?._id }))}>
-                            <Delete />
-                          </IconButton>
-                        </Stack>
-                      </Stack>
-                    </TableCell>
-                  </TableRow>
-                ))
-              ) : (
-                <TableCell colSpan={8}>
-                  <Stack direction="column" alignItems="center" justifyContent="center" height={368} spacing={2}>
-                    <Typography style={{ fontFamily: 'Normal' }}>Tidak ada data.</Typography>
-                  </Stack>
-                </TableCell>
-              )}
-            </TableBody>
-          </Table>
-        </ScrollBar>
-      </TableContainer>
-
-      {listCategory?.data?.length >= 1 && !loading && (
-        <Stack direction="row" alignItems="center" justifyContent="right" spacing={2} mt={2}>
-          <IconButton color="secondary" onClick={() => handlePageChange(filter.page - 1)} disabled={filter.page < 1}>
-            <NavigateBefore />
-          </IconButton>
-          <IconButton
-            color="secondary"
-            onClick={() => handlePageChange(filter.page + 1)}
-            disabled={listCategory?.data?.length < 5}>
-            <NavigateNext />
-          </IconButton>
-        </Stack>
+    <>
+      {showModal.open && (
+        <ModalDeleteCategoryTransaction
+          open={showModal.open}
+          idSelected={filter?.selected}
+          handleClose={() => setShowModal((prevVal) => ({ ...prevVal, open: false, selected: '' }))}
+        />
       )}
-    </Stack>
+
+      <Stack direction="column">
+        <TableContainer component={Paper} style={{ minHeight: 422 }}>
+          <ScrollBar>
+            <Table>
+              <TableHead>
+                <TableRow>
+                  <TableCell>Tanggal Dibuat</TableCell>
+                  <TableCell align="left">Code</TableCell>
+                  <TableCell align="left">COA</TableCell>
+                  <TableCell align="left">Tipe</TableCell>
+                  <TableCell align="left">User</TableCell>
+                  <TableCell align="left"></TableCell>
+                </TableRow>
+              </TableHead>
+
+              <TableBody>
+                {loading ? (
+                  <TableCell colSpan={8}>
+                    <Stack direction="column" alignItems="center" justifyContent="center" height={468} spacing={2}>
+                      <CircularProgress color="secondary" />
+                      <Typography style={{ fontFamily: 'Normal' }}>loading data...</Typography>
+                    </Stack>
+                  </TableCell>
+                ) : listCategory?.data?.length >= 1 ? (
+                  listCategory?.data?.map((item, i) => (
+                    <TableRow key={i} hover>
+                      <TableCell align="left">
+                        <Typography variant="body1" style={{ fontSize: '12px', width: 150 }}>
+                          {moment(item?.createdAt).format('DD/MM/YYYY - HH:mm')} WIB
+                        </Typography>
+                      </TableCell>
+                      <TableCell align="left">
+                        <Typography variant="body1" style={{ fontSize: '12px', width: 120 }}>
+                          {item?.code || '-'}
+                        </Typography>
+                      </TableCell>
+                      <TableCell align="left">
+                        <Typography variant="body1" style={{ fontSize: '12px', width: 120 }}>
+                          {item?.coa || '-'}
+                        </Typography>
+                      </TableCell>
+                      <TableCell align="left">
+                        <Typography variant="body1" style={{ fontSize: '12px', width: 120 }}>
+                          {item?.type?.length >= 1 ? item?.type?.join(', ') : '-'}
+                        </Typography>
+                      </TableCell>
+                      <TableCell align="left">
+                        <Typography variant="body1" style={{ fontSize: '12px', width: 120 }}>
+                          {item?.user || '-'}
+                        </Typography>
+                      </TableCell>
+                      <TableCell align="left">
+                        <Stack direction="row" justifyContent="flex-end">
+                          <Stack direction="row">
+                            <IconButton onClick={() => Router.replace(`/utilitas?tab=bank&bankcode=${item?.bankcode}`)}>
+                              <Edit />
+                            </IconButton>
+                          </Stack>
+                          <Stack direction="row">
+                            <IconButton
+                              onClick={() => setShowModal((prevVal) => ({ ...prevVal, open: true, selected: item?._id }))}>
+                              <Delete />
+                            </IconButton>
+                          </Stack>
+                        </Stack>
+                      </TableCell>
+                    </TableRow>
+                  ))
+                ) : (
+                  <TableCell colSpan={8}>
+                    <Stack direction="column" alignItems="center" justifyContent="center" height={368} spacing={2}>
+                      <Typography style={{ fontFamily: 'Normal' }}>Tidak ada data.</Typography>
+                    </Stack>
+                  </TableCell>
+                )}
+              </TableBody>
+            </Table>
+          </ScrollBar>
+        </TableContainer>
+
+        {listCategory?.data?.length >= 1 && !loading && (
+          <Stack direction="row" alignItems="center" justifyContent="right" spacing={2} mt={2}>
+            <IconButton color="secondary" onClick={() => handlePageChange(filter.page - 1)} disabled={filter.page < 1}>
+              <NavigateBefore />
+            </IconButton>
+            <IconButton
+              color="secondary"
+              onClick={() => handlePageChange(filter.page + 1)}
+              disabled={listCategory?.data?.length < 5}>
+              <NavigateNext />
+            </IconButton>
+          </Stack>
+        )}
+      </Stack>
+    </>
   );
 };
 
