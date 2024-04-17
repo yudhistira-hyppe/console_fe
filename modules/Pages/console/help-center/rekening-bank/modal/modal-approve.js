3de6a2c31500d9e38390d89182e4bc16b2291677
ardihp <ardihp8@gmail.com>
2024-04-17 19:33:59 +0700

    fix(help-center): content detail & modal rekening bank page

--- modules/Pages/console/help-center/rekening-bank/modal/modal-approve.js
+++ modules/Pages/console/help-center/rekening-bank/modal/modal-approve.js
@@ -1,16 +1,15 @@
 import React from 'react';
 import Box from '@mui/material/Box';
-import { Button } from '@material-ui/core';
-import Typography from '@mui/material/Typography';
+import { Typography } from '@material-ui/core';
 import Modal from '@mui/material/Modal';
-import { Stack } from '@mui/material';
+import { Button, Stack } from '@mui/material';
 
 const style = {
   position: 'absolute',
   top: '50%',
   left: '50%',
   transform: 'translate(-50%, -50%)',
-  width: 400,
+  width: 450,
   bgcolor: 'background.paper',
   boxShadow: 24,
   p: '28px 24px',
@@ -22,18 +21,20 @@ export default function ModalApprove({ showModal, onClose, onConfirm, loading })
     <Modal open={showModal} aria-labelledby="modal-modal-title" aria-describedby="modal-modal-description">
       <Box sx={style}>
         <Stack spacing={1} mb={5}>
-          <Typography fontWeight={'bold'}>Kamu Akan Menyetujui Peningkatan Akun</Typography>
-          <Typography variant="body2" color="#666666">
-            Dengan menyetujui kamu yakin akan semua data yang dimasukan sudah benar.
+          <Typography style={{ fontWeight: 'bold', textAlign: 'center', fontSize: 18 }}>
+            Kamu Akan Menyetujui Permohonan Akun Bank
+          </Typography>
+          <Typography style={{ color: '#666666', textAlign: 'center', fontSize: 14 }}>
+            Dengan menyetujui, kamu yakin akan semua data yang dimasukan oleh pengguna sudah benar.
           </Typography>
         </Stack>
 
         <Stack direction={'row'} mt={3} justifyContent={'center'} spacing={3}>
-          <Button variant="contained" color="primary" onClick={onConfirm} disabled={loading}>
-            Konfirmasi
-          </Button>
-          <Button onClick={onClose} disabled={loading}>
+          <Button variant="text" color="inherit" onClick={onClose} disabled={loading}>
             Batal
+          </Button>
+          <Button variant="contained" color="secondary" onClick={onConfirm} disabled={loading}>
+            Konfirmasi
           </Button>
         </Stack>
       </Box>
--- pages/help-center/rekening-bank/detail/index.js
+++ pages/help-center/rekening-bank/detail/index.js
@@ -3,7 +3,7 @@ import dynamic from 'next/dynamic';
 import PageLoader from '@jumbo/components/PageComponents/PageLoader';
 import SecureConsolePage from 'authentication/auth-page-wrappers/SecureConsolePage';
 
-const DetailRekeningBank = dynamic(() => import('modules/Pages/console/help-center/rekening-bank/Detail'), {
+const DetailRekeningBank = dynamic(() => import('modules/Pages/console/help-center/rekening-bank/detail'), {
   loading: () => <PageLoader />,
 });
 
